1d6c47628a5d3962950b73ea0074ba9df9db4cd5
Arne Coucheron <arco68@gmail.com>
2015-08-05 00:14:45 +0200

    squashfs: Add support for LZ4 compression format

--- packages/sysutils/squashfs/package.mk
+++ packages/sysutils/squashfs/package.mk
@@ -23,7 +23,7 @@ PKG_LICENSE="GPL"
 PKG_SITE="http://squashfs.sourceforge.net/"
 PKG_URL="$SOURCEFORGE_SRC/squashfs/squashfs/${PKG_NAME}${PKG_VERSION}/${PKG_NAME}${PKG_VERSION}.tar.gz"
 PKG_SOURCE_DIR="${PKG_NAME}${PKG_VERSION}"
-PKG_DEPENDS_HOST="ccache:host zlib:host lzo:host xz:host"
+PKG_DEPENDS_HOST="ccache:host zlib:host lzo:host xz:host lz4:host"
 PKG_SECTION="sysutils"
 PKG_SHORTDESC="squashfs-tools: A compressed read-only filesystem for Linux"
 PKG_LONGDESC="Squashfs is intended to be a general read-only filesystem, for archival use (i.e. in cases where a .tar.gz file may be used), and in constrained block device/memory systems (e.g. embedded systems) where low overhead is needed. The filesystem is currently stable and has been tested on PowerPC, i386, SPARC and ARM architectures."
@@ -33,7 +33,7 @@ PKG_AUTORECONF="no"
 
 make_host() {
   make -C squashfs-tools mksquashfs \
-       XZ_SUPPORT=1 LZO_SUPPORT=1 \
+       XZ_SUPPORT=1 LZO_SUPPORT=1 LZ4_SUPPORT=1 \
        INCLUDEDIR="-I. -I$ROOT/$TOOLCHAIN/include"
 }
 
