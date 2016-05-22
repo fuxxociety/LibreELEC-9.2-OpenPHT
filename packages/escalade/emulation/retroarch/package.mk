654a812013efd9f0b854602907e9b3ee79b88672
escalade <trondah@gmail.com>
2016-05-22 20:50:11 +0200

    retroarch: update to 02ad990

--- packages/escalade/emulation/retroarch/package.mk
+++ packages/escalade/emulation/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="4af31ce"
+PKG_VERSION="02ad990"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
