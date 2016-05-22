efc67cd34a4d0316880022b225046c8580aac072
escalade <trondah@gmail.com>
2016-05-22 21:02:20 +0200

    snes9x-next-libretro: update to 5d8ab8f

--- packages/escalade/emulation/snes9x-next-libretro/package.mk
+++ packages/escalade/emulation/snes9x-next-libretro/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="snes9x-next-libretro"
-PKG_VERSION="7a7045e"
+PKG_VERSION="5d8ab8f"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Non-commercial"
