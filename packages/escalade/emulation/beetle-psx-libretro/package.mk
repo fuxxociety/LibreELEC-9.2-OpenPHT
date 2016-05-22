5ee790c798e4fbae2161f87d548a9596b6baf38c
escalade <trondah@gmail.com>
2016-05-22 20:53:21 +0200

    beetle-psx-libretro: update to dd6317b

--- packages/escalade/emulation/beetle-psx-libretro/package.mk
+++ packages/escalade/emulation/beetle-psx-libretro/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="beetle-psx-libretro"
-PKG_VERSION="4b4a66e"
+PKG_VERSION="dd6317b"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
