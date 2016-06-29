2c58e6d6582241bdcfd79510f6f85fe526f18179
escalade <trondah@gmail.com>
2016-06-29 20:12:14 +0200

    uae4arm: update to a33d65d

--- packages/escalade/emulation/uae4arm/package.mk
+++ packages/escalade/emulation/uae4arm/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="uae4arm"
-PKG_VERSION="d67a720"
+PKG_VERSION="a33d65d"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
