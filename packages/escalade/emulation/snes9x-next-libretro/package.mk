6b37965ee40e6d782abed56dc8db460d7b7a0221
escalade <trondah@gmail.com>
2016-08-01 20:33:25 +0200

    snes9x-next-libretro: update to efa1eda

--- packages/escalade/emulation/snes9x-next-libretro/package.mk
+++ packages/escalade/emulation/snes9x-next-libretro/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="snes9x-next-libretro"
-PKG_VERSION="5d8ab8f"
+PKG_VERSION="efa1eda"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Non-commercial"
