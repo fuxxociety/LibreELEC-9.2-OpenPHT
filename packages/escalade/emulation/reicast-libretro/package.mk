7cd79b6b61e201ad3cf4cdd5645575a79847f3fe
escalade <trondah@gmail.com>
2016-08-01 20:40:21 +0200

    reicast-libretro: update to a7ff132

--- packages/escalade/emulation/reicast-libretro/package.mk
+++ packages/escalade/emulation/reicast-libretro/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="reicast-libretro"
-PKG_VERSION="846bb24"
+PKG_VERSION="a7ff132"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
