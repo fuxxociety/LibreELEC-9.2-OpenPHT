9bc87ef2ca49cd636e6dd2f09a061e73b26e1c24
escalade <trondah@gmail.com>
2016-08-01 20:28:29 +0200

    mupen64plus-libretro: update to 41c40b8

--- packages/escalade/emulation/mupen64plus-libretro/package.mk
+++ packages/escalade/emulation/mupen64plus-libretro/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="mupen64plus-libretro"
-PKG_VERSION="6f4824f"
+PKG_VERSION="41c40b8"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
