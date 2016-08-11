3f18f027e1b03b0b67861092ec3106f4c19f52d5
escalade <trondah@gmail.com>
2016-08-11 13:26:35 +0200

    oem: modify to install my packages

--- packages/oem/package.mk
+++ packages/oem/package.mk
@@ -22,10 +22,68 @@ PKG_ARCH="any"
 PKG_LICENSE="various"
 PKG_SITE="http://www.libreelec.tv"
 PKG_URL=""
-PKG_DEPENDS_TARGET="toolchain $PROJECT"
+PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="virtual"
 PKG_SHORTDESC="OEM: Metapackage for various OEM packages"
 PKG_LONGDESC="OEM: Metapackage for various OEM packages"
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+
+OEM_APPS_COMMON=" \
+  addons-to-image \
+  docker-system \
+  NzbDrone \
+  plexmediaserver-script \
+  SABnzbd \
+  tcpdump \
+  transmission"
+
+OEM_EMU_COMMON=" \
+  dosbox-sdl \
+  ds4drv \
+  emulationstation \
+  fba-libretro \
+  fuse-libretro \
+  genesis-plus-gx-libretro \
+  emulationstation \
+  mame2003-libretro \
+  mgba-libretro \
+  mupen64plus-libretro \
+  quicknes-libretro \
+  prboom-libretro \
+  retroarch \
+  scraper \
+  scummvm-libretro \
+  snes9x2010-libretro \
+  yabause-libretro \
+  vice"
+
+OEM_EMU_GENERIC=" \
+  beetle-psx-libretro \
+  craft-libretro \
+  desmume-libretro \
+  fs-uae \
+  ppsspp-libretro \
+  qemu \
+  reicast-libretro"
+
+OEM_EMU_RPI=" \
+  uae4arm \
+  pcsx_rearmed-libretro"
+
+if [ ! "$OEM_APPS" = "no" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OEM_APPS_COMMON"
+fi
+
+if [ ! "$OEM_EMU" = "no" ]; then
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OEM_EMU_COMMON"
+  case $PROJECT in
+    Generic)
+      PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OEM_EMU_GENERIC"
+      ;;
+    RPi*)
+      PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OEM_EMU_RPI"
+      ;;
+  esac
+fi
