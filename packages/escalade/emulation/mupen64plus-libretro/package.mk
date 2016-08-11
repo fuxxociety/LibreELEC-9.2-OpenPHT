47d29815d4b572588c278bee8c8a40b61cef5d95
escalade <trondah@gmail.com>
2016-08-11 11:52:33 +0200

    mupen64plus-libretro: update to 625c1fb and add parallel (vulkan)

--- packages/escalade/emulation/mupen64plus-libretro/package.mk
+++ packages/escalade/emulation/mupen64plus-libretro/package.mk
@@ -1,25 +1,23 @@
 ################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
+#      This file is part of LibreELEC - http://www.libreelec.tv
+#      Copyright (C) 2009-2016 Lukas Rusak (lrusak@libreelec.tv)
 #
-#  This Program is free software; you can redistribute it and/or modify
+#  LibreELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2, or (at your option)
-#  any later version.
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
 #
-#  This Program is distributed in the hope that it will be useful,
+#  LibreELEC is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.tv; see the file COPYING.  If not, write to
-#  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
-#  http://www.gnu.org/copyleft/gpl.html
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
 PKG_NAME="mupen64plus-libretro"
-PKG_VERSION="41c40b8"
+PKG_VERSION="625c1fb"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
@@ -28,8 +26,7 @@ PKG_URL="https://github.com/libretro/mupen64plus-libretro/archive/$PKG_VERSION.t
 PKG_DEPENDS_TARGET="toolchain"
 PKG_PRIORITY="optional"
 PKG_SECTION="emulation"
-PKG_SHORTDESC="Libretro GL only. Libretro port of Mupen64 Plus."
-PKG_LONGDESC="Libretro GL only. Libretro port of Mupen64 Plus."
+PKG_SHORTDESC="Libretro port of Mupen64 Plus."
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
@@ -55,11 +52,13 @@ make_target() {
       ;;
     Generic)
       make WITH_DYNAREC=$TARGET_ARCH
+      make WITH_DYNAREC=$TARGET_ARCH HAVE_VULKAN=1
       ;;
   esac
 }
 
 makeinstall_target() {
   mkdir -p $INSTALL/usr/lib/libretro
-  cp mupen64plus_libretro.so $INSTALL/usr/lib/libretro/mupen64plus_libretro.so
+  cp mupen64plus_libretro.so $INSTALL/usr/lib/libretro/
+  cp parallel_libretro.so $INSTALL/usr/lib/libretro/
 }
