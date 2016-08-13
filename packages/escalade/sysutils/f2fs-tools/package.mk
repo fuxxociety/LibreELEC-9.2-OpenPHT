0ec11487f9a4ad12e40a8f28daefdb8008fbbd83
escalade <trondah@gmail.com>
2016-08-13 12:20:47 +0200

    f2fs-tools: update to 1.7.0

--- packages/escalade/sysutils/f2fs-tools/package.mk
+++ packages/escalade/sysutils/f2fs-tools/package.mk
@@ -1,29 +1,29 @@
 ################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+#      This file is part of LibreELEC - http://www.libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
 #
-#  OpenELEC is free software: you can redistribute it and/or modify
+#  LibreELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 2 of the License, or
 #  (at your option) any later version.
 #
-#  OpenELEC is distributed in the hope that it will be useful,
+#  LibreELEC is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
 PKG_NAME="f2fs-tools"
-PKG_VERSION="1.6.1"
+PKG_VERSION="1.7.0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://sourceforge.net/projects/f2fs-tools/"
 PKG_URL="http://git.kernel.org/cgit/linux/kernel/git/jaegeuk/f2fs-tools.git/snapshot/$PKG_NAME-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain util-linux"
+PKG_DEPENDS_TARGET="toolchain util-linux libselinux"
 PKG_DEPENDS_INIT="f2fs-tools"
 PKG_PRIORITY="optional"
 PKG_SECTION="tools"
@@ -33,7 +33,7 @@ PKG_IS_ADDON="no"
 
 PKG_AUTORECONF="yes"
 
-PKG_CONFIGURE_OPTS_TARGET="cross_compiling=maybe BUILD_CC=$HOST_CC \
+PKG_CONFIGURE_OPTS_TARGET="cross_compiling=maybe \
                            --prefix=/usr \
                            --bindir=/bin \
                            --sbindir=/sbin \
