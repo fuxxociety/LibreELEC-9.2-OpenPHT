4f50ec50a7f7f3cf982a5c525bcbb69288444c56
escalade <trondah@gmail.com>
2016-08-13 10:42:07 +0200

    btrfs-progs: update to 4.7

--- packages/escalade/sysutils/btrfs-progs/package.mk
+++ packages/escalade/sysutils/btrfs-progs/package.mk
@@ -1,28 +1,28 @@
 ################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+#      This file is part of LibreELEC - http://www.libreelec.tv
+#      Copyright (C) 2009-2016 Lukas Rusak (lrusak@libreelec.tv)
 #
-#  OpenELEC is free software: you can redistribute it and/or modify
+#  LibreELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 2 of the License, or
 #  (at your option) any later version.
 #
-#  OpenELEC is distributed in the hope that it will be useful,
+#  LibreELEC is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
 PKG_NAME="btrfs-progs"
-PKG_VERSION="350563b"
+PKG_VERSION="v4.7"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/kdave/btrfs-progs"
-PKG_URL="https://github.com/kdave/btrfs-progs/archive/$PKG_VERSION.tar.gz"
+PKG_URL="https://www.kernel.org/pub/linux/kernel/people/kdave/btrfs-progs/btrfs-progs-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain e2fsprogs util-linux zlib lzo"
 PKG_DEPENDS_INIT="btrfs-progs"
 PKG_PRIORITY="optional"
@@ -31,18 +31,15 @@ PKG_SHORTDESC="f2fs-tools: Utilities for use with the f2fs filesystem"
 PKG_LONGDESC="The filesystem utilities for the f2fs filesystem"
 PKG_IS_ADDON="no"
 
-PKG_AUTORECONF="yes"
+PKG_AUTORECONF="no"
 
 PKG_CONFIGURE_OPTS_TARGET="--prefix=/usr \
                            --disable-option-checking \
                            --disable-documentation"
 
 pre_configure_target() {
-  cd ../
-  export PKG_CONFIG_PATH=$SYSROOT_PREFIX/usr/lib/pkgconfig
-  export CFLAGS="$CFLAGS -I$SYSROOT_PREFIX/usr/include"
-  export LDFLAGS="$LDFLAGS -L$SYSROOT_PREFIX/lib"
-  sh autogen.sh
+  cd ..
+  rm -rf $TARGET_NAME
 }
 
 configure_init() {
