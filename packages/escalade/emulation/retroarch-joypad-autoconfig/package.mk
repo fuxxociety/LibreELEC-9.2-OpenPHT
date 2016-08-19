ef997255254281cefeaa2556d3011330b5c43762
escalade <trondah@gmail.com>
2016-08-19 11:13:05 +0000

    retroarch-joypad-autoconfig: update to 06717ae

--- packages/escalade/emulation/retroarch-joypad-autoconfig/package.mk
+++ packages/escalade/emulation/retroarch-joypad-autoconfig/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch-joypad-autoconfig"
-PKG_VERSION="a133ecd"
+PKG_VERSION="06717ae"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
