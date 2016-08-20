b9e1c04642f16cc36743687358373319a3652f93
Rob Loach <robloach@gmail.com>
2016-08-20 11:41:35 -0400

    oem: include beetle-saturn for generic and yabause for arm

--- packages/oem/package.mk
+++ packages/oem/package.mk
@@ -59,11 +59,11 @@ OEM_EMU_COMMON=" \
   scummvm-libretro \
   snes9x2010-libretro \
   tyrquake-libretro \
-  yabause-libretro \
   vice"
 
 OEM_EMU_GENERIC=" \
   beetle-psx-libretro \
+  beetle-saturn-libretro \
   craft-libretro \
   desmume-libretro \
   dolphin \
@@ -74,7 +74,8 @@ OEM_EMU_GENERIC=" \
 
 OEM_EMU_RPI=" \
   uae4arm \
-  pcsx_rearmed-libretro"
+  pcsx_rearmed-libretro \
+  yabause-libretro"
 
 if [ ! "$OEM_APPS" = "no" ]; then
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET $OEM_APPS_COMMON"
