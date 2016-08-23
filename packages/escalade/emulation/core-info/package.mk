2730b38b0f180ba2c365d28bddbe2310b93cf6a5
escalade <trondah@gmail.com>
2016-08-23 11:07:46 +0200

    core-info: update to e419689

--- packages/escalade/emulation/core-info/package.mk
+++ packages/escalade/emulation/core-info/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="core-info"
-PKG_VERSION="2133df2"
+PKG_VERSION="e419689"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
