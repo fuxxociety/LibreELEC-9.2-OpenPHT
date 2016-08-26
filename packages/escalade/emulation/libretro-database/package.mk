9262d5d81b44da8eff831395a6222a990ad920c2
escalade <trondah@gmail.com>
2016-08-26 09:37:19 +0000

    libretro-database: update to d720578

--- packages/escalade/emulation/libretro-database/package.mk
+++ packages/escalade/emulation/libretro-database/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="libretro-database"
-PKG_VERSION="3090ab3"
+PKG_VERSION="d720578"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
