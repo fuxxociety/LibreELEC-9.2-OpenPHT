4a2e7d147e58cf1a088e8cb2a7ef6da2437960ce
escalade <trondah@gmail.com>
2016-08-26 23:41:43 +0200

    mupen64plus-libretro: update to 8103851

--- packages/escalade/emulation/mupen64plus-libretro/package.mk
+++ packages/escalade/emulation/mupen64plus-libretro/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="mupen64plus-libretro"
-PKG_VERSION="625c1fb"
+PKG_VERSION="8103851"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
