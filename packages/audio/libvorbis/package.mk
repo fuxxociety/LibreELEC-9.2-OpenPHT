ac2fe5242223473978d956dfe035ed9b323e4883
escalade <trondah@gmail.com>
2016-08-27 19:01:06 +0200

    Revert "libvorbis: build statically"
    This reverts commit 758f7b554d2d3d9853eb9004854165326f0d2d10.

--- packages/audio/libvorbis/package.mk
+++ packages/audio/libvorbis/package.mk
@@ -31,8 +31,4 @@ PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
 # package specific configure options
-PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared \
-                           --with-ogg=$SYSROOT_PREFIX/usr \
-                           --disable-oggtest \
-                           --disable-docs \
-                           --disable-examples"
+PKG_CONFIGURE_OPTS_TARGET="--with-ogg=$SYSROOT_PREFIX/usr --disable-oggtest --disable-docs --disable-examples"
