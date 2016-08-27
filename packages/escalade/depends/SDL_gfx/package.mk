997e45c7c6181c6fbe0806fc1227eb75dbbd20d5
escalade <trondah@gmail.com>
2016-08-27 17:22:42 +0200

    SDL_gfx: clean up and build statically

--- packages/escalade/depends/SDL_gfx/package.mk
+++ packages/escalade/depends/SDL_gfx/package.mk
@@ -1,19 +1,19 @@
 ################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
+#      This file is part of LibreELEC - https://LibreELEC.tv
+#      Copyright (C) 2016 Team LibreELEC
 #
-#  OpenELEC is free software: you can redistribute it and/or modify
+#  LibreELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 2 of the License, or
 #  (at your option) any later version.
 #
-#  OpenELEC is distributed in the hope that it will be useful,
+#  LibreELEC is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
 PKG_NAME="SDL_gfx"
@@ -32,4 +32,8 @@ PKG_LONGDESC="SDL_image is an image loading library that is used with the SDL li
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-PKG_CONFIGURE_OPTS_TARGET="SDL_CONFIG=$SYSROOT_PREFIX/usr/bin/sdl-config --disable-mmx"
+PKG_CONFIGURE_OPTS_TARGET="--disable-shared --disable-mmx"
+
+pre_configure_target() {
+  export SDL_CONFIG=$SYSROOT_PREFIX/usr/bin/sdl-config
+}
