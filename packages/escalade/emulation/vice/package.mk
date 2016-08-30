027597bc23f30089308c1b97c3856bd1cc0362b2
escalade <trondah@gmail.com>
2016-08-30 12:56:54 +0200

    vice: update to 2.4.29, clean up package and add some fixes

--- packages/escalade/emulation/vice/package.mk
+++ packages/escalade/emulation/vice/package.mk
@@ -1,31 +1,29 @@
 ################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
+#      This file is part of LibreELEC - http://www.libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
 #
-#  This Program is free software; you can redistribute it and/or modify
+#  LibreELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2, or (at your option)
-#  any later version.
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
 #
-#  This Program is distributed in the hope that it will be useful,
+#  LibreELEC is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.tv; see the file COPYING.  If not, write to
-#  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
-#  http://www.gnu.org/copyleft/gpl.html
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
 PKG_NAME="vice"
-PKG_VERSION="2.4.27"
+PKG_VERSION="2.4.29"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
 PKG_SITE="http://vice-emu.sourceforge.net/"
 PKG_URL="https://sourceforge.net/projects/vice-emu/files/development-releases/vice-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain alsa-lib SDL libpng giflib zlib libvorbis"
+PKG_DEPENDS_TARGET="toolchain alsa-lib SDL libpng giflib zlib libvorbis libogg"
 PKG_PRIORITY="optional"
 PKG_SECTION="emulation"
 PKG_SHORTDESC="vice emulator"
@@ -33,12 +31,19 @@ PKG_SHORTDESC="vice emulator"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-PKG_CONFIGURE_OPTS_TARGET="ac_cv_func_SDL_NumJoysticks=yes --prefix=/usr \
+PKG_CONFIGURE_OPTS_TARGET="ac_cv_prog_sdl_config=$SYSROOT_PREFIX/usr/bin/sdl-config \
+			   toolchain_check=no \
+			   --prefix=/usr \
 			   --disable-option-checking \
-			   --enable-native-tools \
+			   --disable-catweasel \
+			   --enable-native-tools=$HOST_CC \
                            --enable-sdlui \
 			   --without-oss \
 			   --without-pulse"
+
+pre_configure_target() {
+  export LIBS="-ludev"
+}
 
 post_makeinstall_target() {
   # copy over scripts
