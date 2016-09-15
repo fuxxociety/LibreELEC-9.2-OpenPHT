51437224fd72c190fac48f5ffa199c51ab9d30e1
escalade <trondah@gmail.com>
2016-09-15 21:26:12 +0000

    vice: update to 2.4.30 and depend on lame for mp3 support

--- packages/escalade/emulation/vice/package.mk
+++ packages/escalade/emulation/vice/package.mk
@@ -17,13 +17,13 @@
 ################################################################################
 
 PKG_NAME="vice"
-PKG_VERSION="2.4.29"
+PKG_VERSION="2.4.30"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
 PKG_SITE="http://vice-emu.sourceforge.net/"
 PKG_URL="https://sourceforge.net/projects/vice-emu/files/development-releases/vice-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain alsa-lib SDL libpng giflib zlib libvorbis libogg"
+PKG_DEPENDS_TARGET="toolchain alsa-lib SDL libpng giflib zlib libvorbis libogg lame"
 PKG_PRIORITY="optional"
 PKG_SECTION="emulation"
 PKG_SHORTDESC="vice emulator"
