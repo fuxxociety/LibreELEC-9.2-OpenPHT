4e6516740f6885591683cdebc7eb3b03a26a895d
escalade <trondah@gmail.com>
2016-09-16 16:26:55 +0000

    SABnzbd: update to 1.1.0

--- packages/escalade/network/SABnzbd/package.mk
+++ packages/escalade/network/SABnzbd/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="SABnzbd"
-PKG_VERSION="1.1.0RC3"
+PKG_VERSION="1.1.0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
