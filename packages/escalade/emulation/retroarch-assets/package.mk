f47c276b06d2b9a89001868a5e6e43251400be51
escalade <trondah@gmail.com>
2016-09-18 13:01:25 +0200

    retroarch-assets: update to 47b809a

--- packages/escalade/emulation/retroarch-assets/package.mk
+++ packages/escalade/emulation/retroarch-assets/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch-assets"
-PKG_VERSION="48267dd"
+PKG_VERSION="47b809a"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
