40ef5a2b0f9fb2c364a3f290bf6c0c9d4ba5eff0
escalade <trondah@gmail.com>
2016-09-27 12:58:45 +0200

    fsuae-libretro: update to 7098440

--- packages/escalade/emulation/fsuae-libretro/package.mk
+++ packages/escalade/emulation/fsuae-libretro/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="fsuae-libretro"
-PKG_VERSION="1be1f74"
+PKG_VERSION="7098440"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
