5e4592b394de8749ac2027d2bda82659bf35762f
escalade <trondah@gmail.com>
2016-09-27 22:09:28 +0200

    craft-libretro: don't use cmake

--- packages/escalade/emulation/craft-libretro/package.mk
+++ packages/escalade/emulation/craft-libretro/package.mk
@@ -1,21 +1,19 @@
 ################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
+#      This file is part of LibreELEC - http://www.libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
 #
-#  This Program is free software; you can redistribute it and/or modify
+#  LibreELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2, or (at your option)
-#  any later version.
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
 #
-#  This Program is distributed in the hope that it will be useful,
+#  LibreELEC is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.tv; see the file COPYING.  If not, write to
-#  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
-#  http://www.gnu.org/copyleft/gpl.html
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
 PKG_NAME="craft-libretro"
@@ -26,21 +24,20 @@ PKG_LICENSE="MAME"
 PKG_SITE="https://github.com/libretro/Craft"
 PKG_URL="https://github.com/libretro/Craft/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain retroarch"
-PKG_PRIORITY="optional"
 PKG_SECTION="emulation"
 PKG_SHORTDESC="libretro Craft core"
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
+PKG_USE_CMAKE="no"
 
-unpack() {
-  tar -zxf $SOURCES/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz -C $BUILD
+post_unpack() {
   mv $BUILD/Craft* $BUILD/$PKG_NAME-$PKG_VERSION
 }
 
 pre_configure_target() {
-  cd $ROOT/$PKG_BUILD
-    rm -rf .$TARGET_NAME
+  cd ..
+  rm -rf .$TARGET_NAME
 }
 
 make_target() {
@@ -55,7 +52,6 @@ make_target() {
       make -f Makefile.libretro
       ;;
   esac
-
 }
 
 makeinstall_target() {
