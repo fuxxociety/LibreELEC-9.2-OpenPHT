f67c0b8ffbe51eb6c37d1fcdf93018bc4aa7a16d
escalade <trondah@gmail.com>
2016-09-28 14:27:56 +0200

    vulkan-loader: clean up and simplify cmake configuration

--- packages/escalade/drivers/vulkan-loader/package.mk
+++ packages/escalade/drivers/vulkan-loader/package.mk
@@ -1,19 +1,19 @@
 ################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+#      This file is part of LibreELEC - http://www.libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
 #
-#  OpenELEC is free software: you can redistribute it and/or modify
+#  LibreELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 2 of the License, or
 #  (at your option) any later version.
 #
-#  OpenELEC is distributed in the hope that it will be useful,
+#  LibreELEC is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
 PKG_NAME="vulkan-loader"
@@ -24,7 +24,6 @@ PKG_LICENSE="Apache 2.0"
 PKG_SITE="https://www.khronos.org"
 PKG_URL="https://github.com/KhronosGroup/Vulkan-LoaderAndValidationLayers/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain cmake:host"
-PKG_PRIORITY="optional"
 PKG_SECTION="depends"
 PKG_SHORTDESC="Vulkan Installable Client Driver (ICD) Loader."
 
@@ -35,26 +34,19 @@ post_unpack() {
   mv $BUILD/Vulkan-Loader* $BUILD/$PKG_NAME-$PKG_VERSION
 }
 
+PKG_CMAKE_OPTS_TARGET="-DBUILD_WSI_XLIB_SUPPORT=On \
+		       -DBUILD_TESTS=Off \
+		       -DBUILD_LAYERS=Off \
+		       -DBUILD_DEMOS=Off \
+		       -DBUILD_VKJSON=Off"
+
 pre_configure_target() {
   cd ..
   ./update_external_sources.sh
-  cd -
-  cmake -DCMAKE_INSTALL_PREFIX=/usr \
-        -DCMAKE_INSTALL_SYSCONFDIR=/etc \
-        -DCMAKE_INSTALL_DATADIR=/share \
-        -DCMAKE_SKIP_RPATH=true \
-        -DBUILD_WSI_XLIB_SUPPORT=On \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DBUILD_TESTS=Off \
-        -DBUILD_LAYERS=Off \
-        -DBUILD_DEMOS=Off \
-        -DBUILD_VKJSON=Off \
-        $EXTRA_CMAKE_OPTS \
-        ..
 }
 
 makeinstall_target() {
   mkdir -p $INSTALL/usr/lib
-  cp -rv loader/libvulkan.so* $INSTALL/usr/lib/
-  cp -rv loader/libvulkan.so* $SYSROOT_PREFIX/usr/lib/
+  cp -r loader/libvulkan.so* $INSTALL/usr/lib/
+  cp -r loader/libvulkan.so* $SYSROOT_PREFIX/usr/lib/
 }
