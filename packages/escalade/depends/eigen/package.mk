4871cbedc42acb48a5d909994e485645a5448c35
escalade <trondah@gmail.com>
2016-09-28 14:25:31 +0200

    eigen: clean up and simplify cmake configuration

--- packages/escalade/depends/eigen/package.mk
+++ packages/escalade/depends/eigen/package.mk
@@ -1,19 +1,19 @@
 ################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+#      This file is part of LibreELEC - http://www.libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
 #
-#  OpenELEC is free software: you can redistribute it and/or modify
+#  LibreELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 2 of the License, or
 #  (at your option) any later version.
 #
-#  OpenELEC is distributed in the hope that it will be useful,
+#  LibreELEC is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
 
 PKG_NAME="eigen"
@@ -23,26 +23,13 @@ PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://eigen.tuxfamily.org/index.php?title=Main_Page"
 PKG_URL="http://bitbucket.org/eigen/eigen/get/$PKG_VERSION.tar.bz2"
-PKG_DEPENDS_TARGET="toolchain systemd cmake:host"
-PKG_PRIORITY="optional"
+PKG_DEPENDS_TARGET="toolchain cmake:host"
 PKG_SECTION="emulators/depends"
 PKG_SHORTDESC="eigen c++ headers"
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-unpack() {
-  tar -jxf $SOURCES/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.bz2 -C $BUILD
+post_unpack() {
   mv $BUILD/eigen-eigen-07105f7124f9 $BUILD/$PKG_NAME-$PKG_VERSION
-}
-
-configure_target() {
-  SYSROOT_PREFIX=$SYSROOT_PREFIX cmake -DCMAKE_TOOLCHAIN_FILE=$CMAKE_CONF \
-        -DCMAKE_INSTALL_PREFIX=/usr \
-        -DCMAKE_INSTALL_LIBDIR=/usr/lib \
-        -DCMAKE_INSTALL_LIBDIR_NOARCH=/usr/lib \
-        -DCMAKE_INSTALL_PREFIX_TOOLCHAIN=$SYSROOT_PREFIX/usr \
-        -DCMAKE_PREFIX_PATH=$SYSROOT_PREFIX/usr \
-        $EXTRA_CMAKE_OPTS \
-        ..
 }
