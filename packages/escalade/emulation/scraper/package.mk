3cdd4921c7cb3f8d072ca7c2bcd0159e5c61d78c
escalade <trondah@gmail.com>
2016-10-04 14:05:34 +0200

    scraper: update to 1.1.10

--- packages/escalade/emulation/scraper/package.mk
+++ packages/escalade/emulation/scraper/package.mk
@@ -1,6 +1,6 @@
 ################################################################################
 #      This file is part of LibreELEC - http://www.libreelec.tv
-#      Copyright (C) 2009-2016 Lukas Rusak (lrusak@libreelec.tv)
+#      Copyright (C) 2016 Team LibreELEC
 #
 #  LibreELEC is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -17,29 +17,23 @@
 ################################################################################
 
 PKG_NAME="scraper"
-PKG_VERSION="1.1.0"
+PKG_VERSION="1.1.10"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/sselph/scraper"
+PKG_URL="custom"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_PRIORITY="optional"
 PKG_SECTION="emulation"
 PKG_SHORTDESC="Docker is an open-source engine that automates the deployment of any application as a lightweight, portable, self-sufficient container that will run virtually anywhere."
 PKG_LONGDESC="Docker containers can encapsulate any payload, and will run consistently on and between virtually any server. The same container that a developer builds and tests on a laptop will run at scale, in production*, on VMs, bare-metal servers, OpenStack clusters, public instances, or combinations of the above."
-PKG_AUTORECONF="no"
 
 PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
 
 unpack() {
-  mkdir -p $ROOT/$PKG_BUILD
-}
-
-configure_target() {
-  :
-}
-
-make_target() {
+  mkdir -p $PKG_BUILD
+  cd $PKG_BUILD
   if [ "$PROJECT" = "RPi" ]; then
     wget $PKG_SITE/releases/download/v$PKG_VERSION/scraper_rpi.zip
   elif [ "$PROJECT" = "RPi2" ]; then
@@ -48,6 +42,11 @@ make_target() {
     wget $PKG_SITE/releases/download/v$PKG_VERSION/scraper_linux_amd64.zip
   fi
   unzip -o *.zip
+  cd $ROOT
+}
+
+make_target() {
+  :
 }
 
 makeinstall_target() {
