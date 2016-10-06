2640b44f4bab7a80e8c1c46f2867129bb1cd6141
escalade <trondah@gmail.com>
2016-10-06 12:28:41 +0200

    retroarch: update to 57f155d

--- packages/escalade/emulation/retroarch/package.mk
+++ packages/escalade/emulation/retroarch/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="1ef2786"
+PKG_VERSION="57f155d"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
