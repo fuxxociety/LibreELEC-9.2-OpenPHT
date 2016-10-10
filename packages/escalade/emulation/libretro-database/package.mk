09e4776244e4dc8f8236558b2e646a4ed897851e
escalade <trondah@gmail.com>
2016-10-10 12:15:22 +0200

    libretro-database: update to 933e4af

--- packages/escalade/emulation/libretro-database/package.mk
+++ packages/escalade/emulation/libretro-database/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="libretro-database"
-PKG_VERSION="96dff5c"
+PKG_VERSION="933e4af"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
