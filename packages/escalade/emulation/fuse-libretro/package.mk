58783aa4ad5a303e43d812e15c81455a88ac6919
escalade <trondah@gmail.com>
2016-10-10 12:10:33 +0200

    fuse-libretro: update to a5b5362

--- packages/escalade/emulation/fuse-libretro/package.mk
+++ packages/escalade/emulation/fuse-libretro/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="fuse-libretro"
-PKG_VERSION="125eae0"
+PKG_VERSION="a5b5362"
 PKG_URL="https://github.com/libretro/fuse-libretro/archive/$PKG_VERSION.tar.gz"
 PKG_REV="1"
 PKG_ARCH="any"
