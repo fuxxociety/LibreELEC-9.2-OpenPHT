2e2f8e8ccc031df3308e888456b6e7d546a57025
escalade <trondah@gmail.com>
2016-10-14 11:13:21 +0000

    fsuae-libretro: update to da2577e

--- packages/escalade/emulation/fsuae-libretro/package.mk
+++ packages/escalade/emulation/fsuae-libretro/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="fsuae-libretro"
-PKG_VERSION="7098440"
+PKG_VERSION="da2577e"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
