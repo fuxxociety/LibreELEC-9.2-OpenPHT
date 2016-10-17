bb1a2ea6a6fea1e33fcb9501ac0079866e2bce45
escalade <trondah@gmail.com>
2016-10-17 20:13:19 +0000

    atk: build shared library

--- packages/addons/addon-depends/atk/package.mk
+++ packages/addons/addon-depends/atk/package.mk
@@ -32,8 +32,7 @@ PKG_IS_ADDON="no"
 
 PKG_AUTORECONF="no"
 
-PKG_CONFIGURE_OPTS_TARGET="--enable-static --disable-shared \
-                           --disable-rebuilds --disable-glibtest"
+PKG_CONFIGURE_OPTS_TARGET="--disable-rebuilds --disable-glibtest"
 
 pre_configure_target() {
   export CFLAGS="$CFLAGS -fPIC"
