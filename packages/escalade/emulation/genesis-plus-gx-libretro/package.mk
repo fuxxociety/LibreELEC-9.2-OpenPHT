7a387abae77159b67aefaeccfe29f18b73261fb1
escalade <trondah@gmail.com>
2016-10-17 20:05:54 +0000

    genesis-plus-gx-libretro: update to 257bb97

--- packages/escalade/emulation/genesis-plus-gx-libretro/package.mk
+++ packages/escalade/emulation/genesis-plus-gx-libretro/package.mk
@@ -19,15 +19,14 @@
 ################################################################################
 
 PKG_NAME="genesis-plus-gx-libretro"
-PKG_VERSION="0bcf4ce"
+PKG_VERSION="257bb97"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Non-commercial"
 PKG_SITE="https://github.com/libretro/Genesis-Plus-GX"
 PKG_URL="https://github.com/libretro/Genesis-Plus-GX/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
-PKG_PRIORITY="optional"
-PKG_SECTION="libretro"
+PKG_SECTION="emulation"
 PKG_SHORTDESC="An enhanced port of Genesis Plus for Gamecube/Wii"
 PKG_LONGDESC="Genesis Plus GX is an open-source & portable Sega Mega Drive / Genesis emulator, now also emulating SG-1000, Master System, Game Gear and Sega/Mega CD hardware."
 
