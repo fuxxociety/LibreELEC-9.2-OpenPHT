ab01936033a4ddf7e55097fecd6a4789de1b3b1c
escalade <trondah@gmail.com>
2016-10-24 14:21:50 +0000

    xfsprogs: update to 4.8.0

--- packages/escalade/sysutils/xfsprogs/package.mk
+++ packages/escalade/sysutils/xfsprogs/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="xfsprogs"
-PKG_VERSION="4.8.0-rc3"
+PKG_VERSION="4.8.0"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
