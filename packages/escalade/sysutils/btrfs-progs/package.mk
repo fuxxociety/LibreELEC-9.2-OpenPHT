1d22083476369b739f2bba5113fa0cf58e5d7cc5
escalade <trondah@gmail.com>
2016-10-28 20:34:25 +0200

    btrfs-progs: update to 4.8.2

--- packages/escalade/sysutils/btrfs-progs/package.mk
+++ packages/escalade/sysutils/btrfs-progs/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="btrfs-progs"
-PKG_VERSION="v4.7.3"
+PKG_VERSION="v4.8.2"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
