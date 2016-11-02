6caf640a6e31451f18748523a5b055d0e398b145
escalade <trondah@gmail.com>
2016-11-02 10:11:29 +0000

    dolphin: update to 6501814

--- packages/escalade/emulation/dolphin/package.mk
+++ packages/escalade/emulation/dolphin/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="dolphin"
-PKG_VERSION="8f0083a"
+PKG_VERSION="6501814"
 PKG_REV="1"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPLv2"
