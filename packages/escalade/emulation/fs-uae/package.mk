e4350e45cb5be7d7b3cddcb432b91b7b209745a5
escalade <trondah@gmail.com>
2016-11-08 23:27:10 +0100

    fs-uae: update to 9a69ecf

--- packages/escalade/emulation/fs-uae/package.mk
+++ packages/escalade/emulation/fs-uae/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="fs-uae"
-PKG_VERSION="d642eaf"
+PKG_VERSION="9a69ecf"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
