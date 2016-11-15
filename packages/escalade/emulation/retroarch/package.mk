3976015e0085ffff525f3ec2a3ab6d4710faa9d9
escalade <trondah@gmail.com>
2016-11-15 14:02:25 +0100

    retroarch: update to 10d925e

--- packages/escalade/emulation/retroarch/package.mk
+++ packages/escalade/emulation/retroarch/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="14f56e1"
+PKG_VERSION="10d925e"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
