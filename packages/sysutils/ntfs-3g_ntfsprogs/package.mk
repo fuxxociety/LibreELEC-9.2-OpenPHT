e1cb8badce86b7b4c076fd84a4f3d5def66f7ef3
escalade <trondah@gmail.com>
2016-11-20 17:30:03 +0000

    ntfs-3g_ntfsprogs: include tools

--- packages/sysutils/ntfs-3g_ntfsprogs/package.mk
+++ packages/sysutils/ntfs-3g_ntfsprogs/package.mk
@@ -41,18 +41,6 @@ PKG_CONFIGURE_OPTS_TARGET="--exec-prefix=/usr/ \
                            --with-uuid"
 
 post_makeinstall_target() {
-  # dont include ntfsprogs.
-  for i in $INSTALL/usr/bin/*; do
-    if [ "$(basename $i)" != "ntfs-3g" ]; then
-      rm $i
-    fi
-  done
-
-  rm -rf $INSTALL/sbin
-  rm -rf $INSTALL/usr/sbin/ntfsclone
-  rm -rf $INSTALL/usr/sbin/ntfscp
-  rm -rf $INSTALL/usr/sbin/ntfsundelete
-
   mkdir -p $INSTALL/usr/sbin
     ln -sf /usr/bin/ntfs-3g $INSTALL/usr/sbin/mount.ntfs
     ln -sf /usr/sbin/mkntfs $INSTALL/usr/sbin/mkfs.ntfs
