3f727025ecaaf6e4e45caba6b4be7874a71af99f
escalade <trondah@gmail.com>
2016-11-22 23:33:59 +0000

    vulkan-loader: update to 03359bf

--- packages/escalade/drivers/vulkan-loader/package.mk
+++ packages/escalade/drivers/vulkan-loader/package.mk
@@ -17,22 +17,19 @@
 ################################################################################
 
 PKG_NAME="vulkan-loader"
-PKG_VERSION="5c6aacf"
+PKG_VERSION="03359bf"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Apache 2.0"
 PKG_SITE="https://www.khronos.org"
 PKG_URL="https://github.com/KhronosGroup/Vulkan-LoaderAndValidationLayers/archive/$PKG_VERSION.tar.gz"
+PKG_SOURCE_DIR="Vulkan-*$PKG_VERSION*"
 PKG_DEPENDS_TARGET="toolchain cmake:host"
 PKG_SECTION="depends"
 PKG_SHORTDESC="Vulkan Installable Client Driver (ICD) Loader."
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
-
-post_unpack() {
-  mv $BUILD/Vulkan-Loader* $BUILD/$PKG_NAME-$PKG_VERSION
-}
 
 PKG_CMAKE_OPTS_TARGET="-DBUILD_WSI_XLIB_SUPPORT=On \
 		       -DBUILD_TESTS=Off \
