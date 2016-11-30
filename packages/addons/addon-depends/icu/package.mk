7f99359e1f112efb8f2d58a420220847f51e8ee3
escalade <trondah@gmail.com>
2016-11-30 14:29:38 +0100

    icu: build shared library and modify for target install

--- packages/addons/addon-depends/icu/package.mk
+++ packages/addons/addon-depends/icu/package.mk
@@ -31,14 +31,6 @@ PKG_LONGDESC="International Components for Unicode library"
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared"
-
-PKG_CONFIGURE_OPTS_TARGET="--enable-static \
-                           --disable-shared \
-                           --with-cross-build=$ROOT/$PKG_BUILD/.$HOST_NAME"
+PKG_CONFIGURE_OPTS_TARGET="--with-cross-build=$ROOT/$PKG_BUILD/.$HOST_NAME"
 
 PKG_CONFIGURE_SCRIPT="source/configure"
-
-post_makeinstall_target() {
-  rm -rf $INSTALL
-}
