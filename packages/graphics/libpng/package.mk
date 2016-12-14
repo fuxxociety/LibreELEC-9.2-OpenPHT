27e6b78968d72cc90e73d2befff9a8eea63e0a31
escalade <trondah@gmail.com>
2016-12-14 09:56:28 +0100

    libpng: build shared library

--- packages/graphics/libpng/package.mk
+++ packages/graphics/libpng/package.mk
@@ -32,9 +32,7 @@ PKG_IS_ADDON="no"
 PKG_USE_CMAKE="no"
 PKG_AUTORECONF="no"
 
-PKG_CONFIGURE_OPTS_TARGET="ac_cv_lib_z_zlibVersion=yes \
-                           --enable-static \
-                           --disable-shared"
+PKG_CONFIGURE_OPTS_TARGET="ac_cv_lib_z_zlibVersion=yes"
 
 PKG_CONFIGURE_OPTS_HOST="--enable-static --disable-shared"
 
