1210f1208c43d212ee6503cb7b3a6403da462be8
escalade <trondah@gmail.com>
2016-12-15 15:37:04 +0100

    libretro-database: update to 1e104c8

--- packages/escalade/emulation/libretro-database/package.mk
+++ packages/escalade/emulation/libretro-database/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="libretro-database"
-PKG_VERSION="d812a19"
+PKG_VERSION="1e104c8"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
