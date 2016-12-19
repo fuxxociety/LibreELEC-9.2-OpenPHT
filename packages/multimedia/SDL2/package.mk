e90de612f7ba185781a36e0ecef45d3f12199de8
escalade <trondah@gmail.com>
2016-12-19 13:22:10 +0100

    SDL2: update to 951286c

--- packages/multimedia/SDL2/package.mk
+++ packages/multimedia/SDL2/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="SDL2"
-PKG_VERSION="1886965"
+PKG_VERSION="951286c"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.libsdl.org/"
