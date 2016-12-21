17818c66ec772ced9dbcd4868996265fb214a7dc
escalade <trondah@gmail.com>
2016-12-21 22:46:56 +0100

    retroarch: update to 2576d9e

--- packages/escalade/emulation/retroarch/package.mk
+++ packages/escalade/emulation/retroarch/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="8982022"
+PKG_VERSION="2576d9e"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
