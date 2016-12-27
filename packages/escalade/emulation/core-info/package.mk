f77c0d649218f55398d90b7caaa0d67e043373fa
escalade <trondah@gmail.com>
2016-12-27 20:24:21 +0100

    core-info: update to 83b074e

--- packages/escalade/emulation/core-info/package.mk
+++ packages/escalade/emulation/core-info/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="core-info"
-PKG_VERSION="f751642"
+PKG_VERSION="83b074e"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
