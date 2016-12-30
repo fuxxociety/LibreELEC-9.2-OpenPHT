4bbdc375d384ebf35ca3a3ff1497ffb60175ba18
escalade <trondah@gmail.com>
2016-12-30 02:07:03 +0000

    retroarch: update to 97e7582

--- packages/escalade/emulation/retroarch/package.mk
+++ packages/escalade/emulation/retroarch/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="c6269e3"
+PKG_VERSION="97e7582"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
