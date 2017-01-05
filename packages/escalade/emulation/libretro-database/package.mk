dbc4cc8301ea8cf46bdf6792beff0c6a6817c94d
escalade <trondah@gmail.com>
2017-01-05 08:33:56 +0100

    libretro-database: update to f5e33e9

--- packages/escalade/emulation/libretro-database/package.mk
+++ packages/escalade/emulation/libretro-database/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="libretro-database"
-PKG_VERSION="1e104c8"
+PKG_VERSION="f5e33e9"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
