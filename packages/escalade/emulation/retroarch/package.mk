46f8f10c088b651fb85f5e39b9cc0ef1d216f7b2
escalade <trondah@gmail.com>
2017-01-05 08:39:00 +0100

    retroarch: update to d49502d

--- packages/escalade/emulation/retroarch/package.mk
+++ packages/escalade/emulation/retroarch/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="97e7582"
+PKG_VERSION="d49502d"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
