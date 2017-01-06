4dbb8da4523dee6e202aee04402b24581f970778
escalade <trondah@gmail.com>
2017-01-06 19:52:16 +0100

    retroarch: update to 9d7fe2c

--- packages/escalade/emulation/retroarch/package.mk
+++ packages/escalade/emulation/retroarch/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="d49502d"
+PKG_VERSION="9d7fe2c"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
