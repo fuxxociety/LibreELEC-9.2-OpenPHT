107fce104cfd2b6c7aa64ea958bad0b9e40495c7
escalade <trondah@gmail.com>
2017-01-16 12:59:34 +0000

    mesa: update to 7a2b65a

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="mesa"
-PKG_VERSION="dd39e48"
+PKG_VERSION="7a2b65a"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
