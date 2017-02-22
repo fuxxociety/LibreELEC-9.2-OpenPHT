9407a51d5bc08672dcfe97c8e011b79fa9f25111
escalade <trondah@gmail.com>
2017-02-22 07:59:41 +0000

    openssl-compat: update to 1.0.2k, refresh package according to the regular openssl package (without CFLAGS passed to Configure) and rename libs to lib*-compat.so

--- packages/escalade/depends/openssl-compat/package.mk
+++ packages/escalade/depends/openssl-compat/package.mk
@@ -1,5 +1,5 @@
-################################################################################
-#      This file is part of LibreELEC - http://www.libreelec.tv
+###############################################################################
+#      This file is part of LibreELEC - https://libreelec.tv
 #      Copyright (C) 2016 Team LibreELEC
 #
 #  LibreELEC is free software: you can redistribute it and/or modify
@@ -17,75 +17,71 @@
 ################################################################################
 
 PKG_NAME="openssl-compat"
-PKG_VERSION="1.0.2j"
+PKG_VERSION="1.0.2k"
+PKG_REV="1"
 PKG_ARCH="any"
-PKG_LICENSE="OSS"
-PKG_SITE="http://www.openssl.org/"
-PKG_URL="http://www.openssl.org/source/openssl-$PKG_VERSION.tar.gz"
+PKG_LICENSE="BSD"
+PKG_SITE="https://www.openssl.org"
+PKG_URL="https://www.openssl.org/source/openssl-$PKG_VERSION.tar.gz"
 PKG_SOURCE_DIR="openssl-$PKG_VERSION"
-PKG_DEPENDS_TARGET="toolchain zlib"
+PKG_DEPENDS_HOST="ccache:host"
+PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="security"
-PKG_SHORTDESC="openssl: Open Source toolkit for Secure Sockets Layer and Transport Layer Security"
-PKG_LONGDESC="The OpenSSL Project is a collaborative effort to develop a robust, commercial-grade, fully featured, and Open Source toolkit implementing the Secure Sockets Layer (SSL v2/v3) and Transport Layer Security (TLS v1) protocols as well as a full-strength general purpose cryptography library. The project is managed by a worldwide community of volunteers that use the Internet to communicate, plan, and develop the OpenSSL toolkit and its related documentation."
+PKG_SHORTDESC="The Open Source toolkit for Secure Sockets Layer and Transport Layer Security"
+PKG_LONGDESC="The Open Source toolkit for Secure Sockets Layer and Transport Layer Security"
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
+PKG_CONFIGURE_OPTS_SHARED="--openssldir=/etc/ssl \
+                           --libdir=lib \
+                           shared \
+                           threads \
+                           no-ec2m \
+                           no-gmp \
+                           no-jpake \
+                           no-krb5 \
+                           no-libunbound \
+                           no-md2 \
+                           no-rc5 \
+                           no-rfc3779
+                           no-sctp \
+                           no-ssl-trace \
+                           no-ssl2 \
+                           no-ssl3 \
+                           no-store \
+                           no-unit-test \
+                           no-weak-ssl-ciphers \
+                           no-zlib \
+                           no-zlib-dynamic \
+                           no-static-engine"
+
 pre_configure_target() {
   export MAKEFLAGS=-j1
+  mkdir -p $ROOT/$PKG_BUILD/.$TARGET_NAME
+  cp -a $ROOT/$PKG_BUILD/* $ROOT/$PKG_BUILD/.$TARGET_NAME/
 
   case $TARGET_ARCH in
     x86_64)
       OPENSSL_TARGET=linux-x86_64
+      PLATFORM_FLAGS=enable-ec_nistp_64_gcc_128
       ;;
     arm)
-      OPENSSL_TARGET=linux-armv7
+      OPENSSL_TARGET=linux-armv4
+      ;;
+    aarch64)
+      OPENSSL_TARGET=linux-aarch64
       ;;
   esac
 }
 
 configure_target() {
-  ./Configure --prefix=/usr \
-              --libdir=lib \
-              --openssldir="/etc/ssl" \
-              shared \
-              threads \
-              zlib \
-              enable-camellia \
-              enable-seed \
-              enable-tlsext \
-              enable-rfc3779 \
-              enable-cms \
-              enable-md2 \
-              no-krb5 \
-              no-mdc2 \
-              no-rc5 \
-              no-ec \
-              no-ec2m \
-              no-ecdh \
-              no-ecdsa \
-              no-srp \
-              $OPENSSL_TARGET
-}
-
-make_target() {
-  make CC="$CC" \
-       AR="$AR r" \
-       RANLIB="$RANLIB" \
-       MAKEDEPPROG="$CC" \
-       depend all build-shared
-
+  cd $ROOT/$PKG_BUILD/.$TARGET_NAME
+  ./Configure --prefix=/usr $PKG_CONFIGURE_OPTS_SHARED $PLATFORM_FLAGS $OPENSSL_TARGET
 }
 
 makeinstall_target() {
-  make INSTALL_PREFIX=$INSTALL install
-  chmod 755 $INSTALL/usr/lib/*.so*
-  chmod 755 $INSTALL/usr/lib/engines/*.so
-}
-
-post_makeinstall_target() {
-  rm -rf $INSTALL/etc
-  rm -rf $INSTALL/usr/bin
-  rm -rf $INSTALL/usr/lib/engines
-  rm -rf $INSTALL/usr/lib/*.so
+  mkdir -p $INSTALL/usr/lib
+  cp libcrypto.so $INSTALL/usr/lib/libcrypto-compat.so
+  cp libssl.so $INSTALL/usr/lib/libssl-compat.so
 }
