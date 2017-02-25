66ad194ae6b5888a0653c8e3168b5620a369bb13
escalade <trondah@gmail.com>
2017-02-25 14:11:25 +0100

    sonarr: update to 2.0.0.4613

--- packages/escalade/network/sonarr/package.mk
+++ packages/escalade/network/sonarr/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="sonarr"
-PKG_VERSION="2.0.0.4409"
+PKG_VERSION="2.0.0.4613"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://sonarr.tv"
