d206341d66f75d78d3e50f4399a5dab8d8101518
escalade <trondah@gmail.com>
2017-02-27 21:46:06 +0000

    vulkan-loader: update to 1.0.39.1

--- packages/escalade/drivers/vulkan-loader/package.mk
+++ packages/escalade/drivers/vulkan-loader/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="vulkan-loader"
-PKG_VERSION="1.0.39.0"
+PKG_VERSION="1.0.39.1"
 PKG_ARCH="any"
 PKG_LICENSE="Apache 2.0"
 PKG_SITE="https://www.khronos.org"
