ed1be132979a6d6a2d86fea69ba75ee0eb604384
escalade <trondah@gmail.com>
2017-02-27 21:53:54 +0000

    snes9x2010-libretro: update to b2a69de

--- packages/escalade/emulation/snes9x2010-libretro/package.mk
+++ packages/escalade/emulation/snes9x2010-libretro/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="snes9x2010-libretro"
-PKG_VERSION="74953d8"
+PKG_VERSION="b2a69de"
 PKG_ARCH="any"
 PKG_LICENSE="Non-commercial"
 PKG_SITE="https://github.com/libretro/snes9x2010.git"
