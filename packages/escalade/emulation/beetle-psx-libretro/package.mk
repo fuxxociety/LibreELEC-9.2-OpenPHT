f60d7668b3758a6c4671080d628a5fe0dfb7f10d
escalade <trondah@gmail.com>
2017-02-27 21:51:59 +0000

    beetle-psx-libretro: update to 99747d4

--- packages/escalade/emulation/beetle-psx-libretro/package.mk
+++ packages/escalade/emulation/beetle-psx-libretro/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="beetle-psx-libretro"
-PKG_VERSION="f12cc2f"
+PKG_VERSION="99747d4"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/beetle-psx-libretro"
