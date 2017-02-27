d9048da5092a49fb9c0de463a35f4f5216efeece
escalade <trondah@gmail.com>
2017-02-27 21:56:05 +0000

    retroarch: update to 15001a8

--- packages/escalade/emulation/retroarch/package.mk
+++ packages/escalade/emulation/retroarch/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="8114ce7"
+PKG_VERSION="15001a8"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/RetroArch.git"
