81d8b317c200898acec0fa50ac35ba4ebd019911
escalade <trondah@gmail.com>
2017-02-27 21:55:25 +0000

    libretro-database: update to d05d520

--- packages/escalade/emulation/libretro-database/package.mk
+++ packages/escalade/emulation/libretro-database/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="libretro-database"
-PKG_VERSION="f5e33e9"
+PKG_VERSION="d05d520"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/libretro/libretro-database"
