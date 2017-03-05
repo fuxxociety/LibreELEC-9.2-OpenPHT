e3674e65d0aa22178fce067afbd4e4c0de4dd804
escalade <trondah@gmail.com>
2017-03-05 14:35:09 +0000

    retroarch: update to e587264

--- packages/escalade/emulation/retroarch/package.mk
+++ packages/escalade/emulation/retroarch/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="15001a8"
+PKG_VERSION="e587264"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/RetroArch.git"
