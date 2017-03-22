1a8eda7508a65d046183984e00c811d2653bcbd1
escalade <trondah@gmail.com>
2017-03-22 13:23:28 +0000

    openvpn: update to 2.4.1

--- packages/network/openvpn/package.mk
+++ packages/network/openvpn/package.mk
@@ -18,18 +18,18 @@
 ################################################################################
 
 PKG_NAME="openvpn"
-PKG_VERSION="2.4.0"
+PKG_VERSION="2.4.1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://openvpn.net"
-PKG_URL="http://swupdate.openvpn.org/community/releases/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_URL="https://github.com/OpenVPN/openvpn/archive/v$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain lzo libressl"
 PKG_SECTION="network"
 PKG_SHORTDESC="openvpn: a full featured SSL VPN software solution that integrates OpenVPN server capabilities."
 PKG_LONGDESC="OpenVPN Access Server is a full featured SSL VPN software solution that integrates OpenVPN server capabilities, enterprise management capabilities, simplified OpenVPN Connect UI, and OpenVPN Client software packages that accommodate Windows, MAC, and Linux OS environments. OpenVPN Access Server supports a wide range of configurations, including secure and granular remote access to internal network and/ or private cloud network resources and applications with fine-grained access control."
 
 PKG_IS_ADDON="no"
-PKG_AUTORECONF="no"
+PKG_AUTORECONF="yes"
 
 PKG_CONFIGURE_OPTS_TARGET="ac_cv_have_decl_TUNSETPERSIST=no \
                            --enable-password-save \
