96c071561795256d0b4de58b4d1cc268982a2cbb
escalade <trondah@gmail.com>
2017-03-23 01:34:24 +0000

    dolphin: update to 9ea5913

--- packages/escalade/emulation/dolphin/package.mk
+++ packages/escalade/emulation/dolphin/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="dolphin"
-PKG_VERSION="883bec8"
+PKG_VERSION="9ea5913"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/dolphin-emu/dolphin"
