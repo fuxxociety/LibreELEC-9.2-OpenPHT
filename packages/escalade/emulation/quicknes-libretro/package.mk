efe432e2c5996acf2106a9e83826a3c332a36c2b
escalade <trondah@gmail.com>
2017-03-27 02:41:47 +0200

    quicknes-libretro: update to 8613b48 and fix gitversion

--- packages/escalade/emulation/quicknes-libretro/package.mk
+++ packages/escalade/emulation/quicknes-libretro/package.mk
@@ -19,11 +19,12 @@
 ################################################################################
 
 PKG_NAME="quicknes-libretro"
-PKG_VERSION="55379b9"
+PKG_VERSION="8613b48"
 PKG_ARCH="any"
 PKG_LICENSE="LGPLv2.1+"
 PKG_SITE="https://github.com/libretro/QuickNES_Core"
 PKG_URL="https://github.com/libretro/QuickNES_Core/archive/$PKG_VERSION.tar.gz"
+PKG_SOURCE_DIR="QuickNES_Core-$PKG_VERSION*"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="emulation"
 PKG_SHORTDESC="The QuickNES core library, originally by Shay Green, heavily modified"
@@ -32,9 +33,8 @@ PKG_LONGDESC="The QuickNES core library, originally by Shay Green, heavily modif
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-unpack() {
-  tar -zxf $SOURCES/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz -C $BUILD
-  mv $BUILD/QuickNES* $BUILD/$PKG_NAME-$PKG_VERSION
+pre_build_target() {
+  export GIT_VERSION=$PKG_VERSION
 }
 
 makeinstall_target() {
