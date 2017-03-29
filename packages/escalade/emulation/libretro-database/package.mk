5683c23c344ca2dc2f659d77ed13b0606ca2ad29
escalade <trondah@gmail.com>
2017-03-29 12:52:25 +0200

    libretro-database: update to 43a1e01

--- packages/escalade/emulation/libretro-database/package.mk
+++ packages/escalade/emulation/libretro-database/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="libretro-database"
-PKG_VERSION="d05d520"
+PKG_VERSION="43a1e01"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/libretro/libretro-database"
