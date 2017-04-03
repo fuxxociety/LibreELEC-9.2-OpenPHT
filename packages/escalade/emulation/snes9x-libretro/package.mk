0bed0ef78f46b0d62c249ce4e1df781155b909b4
escalade <trondah@gmail.com>
2017-04-03 21:04:23 +0200

    snes9x-libretro: initial package

---
+++ packages/escalade/emulation/snes9x-libretro/package.mk
@@ -0,0 +1,44 @@
+################################################################################
+#      This file is part of LibreELEC - http://www.libreelec.tv
+#      Copyright (C) 2016 Team LibreELEC
+#
+#  LibreELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  LibreELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="snes9x-libretro"
+PKG_VERSION="0240276"
+PKG_ARCH="any"
+PKG_LICENSE="Non-commercial"
+PKG_SITE="https://github.com/libretro/snes9x"
+PKG_URL="https://github.com/libretro/snes9x/archive/$PKG_VERSION.tar.gz"
+PKG_SOURCE_DIR="snes9x-$PKG_VERSION*"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="emulation"
+PKG_SHORTDESC="Snes9x - Portable Super Nintendo Entertainment System (TM) emulator"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+pre_build_target() {
+  export GIT_VERSION=$PKG_VERSION
+}
+
+make_target() {
+  make -C libretro
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/libretro
+  cp libretro/*.so $INSTALL/usr/lib/libretro/
+}
