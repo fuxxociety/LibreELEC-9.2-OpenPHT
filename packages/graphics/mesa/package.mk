1469d7e2e5c602e695ab60d8a92889a0a07f20b3
escalade <trondah@gmail.com>
2017-05-02 17:20:41 +0000

    mesa: update to 72e52fa

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="mesa"
-PKG_VERSION="a2eae66"
+PKG_VERSION="72e52fa"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
