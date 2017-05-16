f1fdbbbc3632a6861e17295907023171c5c6e841
escalade <trondah@gmail.com>
2017-05-16 19:17:09 +0000

    retroarch: update to 808b973

--- packages/escalade/emulation/retroarch/package.mk
+++ packages/escalade/emulation/retroarch/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="da1ab24"
+PKG_VERSION="808b973"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/RetroArch.git"
