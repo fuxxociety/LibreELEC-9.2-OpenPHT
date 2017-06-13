1976244c015839e428cfab79a5bab85ab09ee070
escalade <trondah@gmail.com>
2017-06-13 08:38:31 +0000

    retroarch: update to 2496a62

--- packages/escalade/emulation/retroarch/package.mk
+++ packages/escalade/emulation/retroarch/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="0c32835"
+PKG_VERSION="2496a62"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/RetroArch.git"
