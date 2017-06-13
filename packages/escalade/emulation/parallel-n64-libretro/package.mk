eda7f7cc488ab561c50a8cf27e8d0168e5aa7b50
escalade <trondah@gmail.com>
2017-06-13 08:37:26 +0000

    parallel-n64-libretro: update to 19486a5

--- packages/escalade/emulation/parallel-n64-libretro/package.mk
+++ packages/escalade/emulation/parallel-n64-libretro/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="parallel-n64-libretro"
-PKG_VERSION="571a477"
+PKG_VERSION="19486a5"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/parallel-n64"
