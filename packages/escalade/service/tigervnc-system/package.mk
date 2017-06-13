a3a49f1168ffe1b360b5507103135174b22b1c84
escalade <trondah@gmail.com>
2017-06-13 08:38:49 +0000

    tigervnc-system: update to 1.8.0

--- packages/escalade/service/tigervnc-system/package.mk
+++ packages/escalade/service/tigervnc-system/package.mk
@@ -23,14 +23,14 @@ PKG_LICENSE="GPLv2"
 PKG_SITE="http://www.tigervnc.org"
 PKG_URL="https://github.com/TigerVNC/tigervnc/archive/v$PKG_VERSION.tar.gz"
 PKG_SOURCE_DIR="tigervnc-$PKG_VERSION"
-PKG_DEPENDS_TARGET="toolchain cmake:host libX11 libXext libXtst zlib libjpeg-turbo"
+PKG_DEPENDS_TARGET="toolchain cmake:host libX11 libXdamage libXext libXtst zlib libjpeg-turbo"
 PKG_SECTION="service"
 PKG_SHORTDESC="TigerVNC server"
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-PKG_CMAKE_OPTS_TARGET="-DBUILD_VIEWER=off"
+PKG_CMAKE_OPTS_TARGET="-DBUILD_VIEWER=off -Wno-dev"
 
 post_makeinstall_target() {
   rm $INSTALL/usr/bin/vncserver
