9d1ab6c63e9b7594a39c4d7c030e8464456bdcb5
escalade <trondah@gmail.com>
2017-07-05 14:25:51 +0200

    tyrquake-libretro: update to a0502d1

--- packages/escalade/emulation/tyrquake-libretro/package.mk
+++ packages/escalade/emulation/tyrquake-libretro/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="tyrquake-libretro"
-PKG_VERSION="7f3c540"
+PKG_VERSION="a0502d1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/tyrquake"
