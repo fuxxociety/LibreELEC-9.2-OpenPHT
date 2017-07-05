0c78d58b99a9a91dcae4d6216c228c5e3cb290c9
escalade <trondah@gmail.com>
2017-07-05 13:23:21 +0200

    cryptography: update to 1.9

--- packages/escalade/python/cryptography/package.mk
+++ packages/escalade/python/cryptography/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="cryptography"
-PKG_VERSION="1.7.1"
+PKG_VERSION="1.9"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="https://github.com/pyca/cryptography"
