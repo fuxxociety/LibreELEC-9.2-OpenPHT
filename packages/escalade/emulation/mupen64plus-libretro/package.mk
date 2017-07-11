63689eb04edcc1daecba5e1c4f4583384274f8b5
escalade <trondah@gmail.com>
2017-07-11 23:23:24 +0200

    mupen64plus-libretro: update to 2a7893e

--- packages/escalade/emulation/mupen64plus-libretro/package.mk
+++ packages/escalade/emulation/mupen64plus-libretro/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="mupen64plus-libretro"
-PKG_VERSION="eda9d16"
+PKG_VERSION="2a7893e"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/mupen64plus-libretro"
