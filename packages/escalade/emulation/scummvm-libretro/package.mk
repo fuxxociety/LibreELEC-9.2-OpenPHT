200935f49857e0c0d99ff18c5b0a1cfe43254cde
escalade <trondah@gmail.com>
2017-07-25 11:29:14 +0200

    scummvm-libretro: update to fbbe839

--- packages/escalade/emulation/scummvm-libretro/package.mk
+++ packages/escalade/emulation/scummvm-libretro/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="scummvm-libretro"
-PKG_VERSION="de8d7e5"
+PKG_VERSION="fbbe839"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/scummvm"
