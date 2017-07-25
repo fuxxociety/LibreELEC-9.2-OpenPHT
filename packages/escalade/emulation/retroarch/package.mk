19491c8ac3b657f24d5c81f3f7f9798f841711e1
escalade <trondah@gmail.com>
2017-07-25 11:30:02 +0200

    retroarch: update to 6f637df

--- packages/escalade/emulation/retroarch/package.mk
+++ packages/escalade/emulation/retroarch/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="dc521b4"
+PKG_VERSION="6f637df"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/RetroArch.git"
