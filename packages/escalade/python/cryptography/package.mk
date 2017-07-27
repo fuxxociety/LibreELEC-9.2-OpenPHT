6fc0e1e18975c79d84476003edae4bf238a09cf3
escalade <trondah@gmail.com>
2017-07-27 23:19:00 +0200

    cryptography: update to 2.0.2

--- packages/escalade/python/cryptography/package.mk
+++ packages/escalade/python/cryptography/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="cryptography"
-PKG_VERSION="2.0"
+PKG_VERSION="2.0.2"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="https://github.com/pyca/cryptography"
