f25f461c22192b849250df2a77a605fbd8bf9495
escalade <trondah@gmail.com>
2017-08-01 00:11:23 +0200

    snes9x2010-libretro: update to 26fbc4b

--- packages/escalade/emulation/snes9x2010-libretro/package.mk
+++ packages/escalade/emulation/snes9x2010-libretro/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="snes9x2010-libretro"
-PKG_VERSION="b2a69de"
+PKG_VERSION="26fbc4b"
 PKG_ARCH="any"
 PKG_LICENSE="Non-commercial"
 PKG_SITE="https://github.com/libretro/snes9x2010.git"
