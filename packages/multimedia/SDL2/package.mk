efa4783a166c7d71549b6e4edd18577e36950ff3
escalade <trondah@gmail.com>
2017-08-22 15:28:43 +0000

    SDL2: update to d4564ef

--- packages/multimedia/SDL2/package.mk
+++ packages/multimedia/SDL2/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="SDL2"
-PKG_VERSION="59c39da"
+PKG_VERSION="d4564ef"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.libsdl.org/"
