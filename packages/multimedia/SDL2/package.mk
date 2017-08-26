7eb23c7c1e187d8a11d4f90e76826cb3c4f46f32
escalade <trondah@gmail.com>
2017-08-26 19:14:02 +0000

    Revert "SDL2: update to d4564ef"
    This reverts commit efa4783a166c7d71549b6e4edd18577e36950ff3.

--- packages/multimedia/SDL2/package.mk
+++ packages/multimedia/SDL2/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="SDL2"
-PKG_VERSION="d4564ef"
+PKG_VERSION="59c39da"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.libsdl.org/"
