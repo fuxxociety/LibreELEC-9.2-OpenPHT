50385f43eea71a076f3a38d80858b74cabe27405
escalade <trondah@gmail.com>
2017-08-29 01:01:56 +0200

    ncurses: update to 6.0-20170827

--- packages/devel/ncurses/package.mk
+++ packages/devel/ncurses/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="ncurses"
-PKG_VERSION="6.0-20170729"
+PKG_VERSION="6.0-20170827"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
