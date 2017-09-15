25ada663ce9c7c447133414f2fa72f1362e74118
escalade <trondah@gmail.com>
2017-09-15 12:45:01 +0200

    parallel-n64-libretro: update to c052e43

--- packages/escalade/emulation/parallel-n64-libretro/package.mk
+++ packages/escalade/emulation/parallel-n64-libretro/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="parallel-n64-libretro"
-PKG_VERSION="1662062"
+PKG_VERSION="c052e43"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/parallel-n64"
