ce691edcbca70222c491294324e24fd1f575b989
escalade <trondah@gmail.com>
2017-09-19 00:04:08 +0200

    Python: update to 2.7.14

--- packages/lang/Python/package.mk
+++ packages/lang/Python/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="Python"
-PKG_VERSION="2.7.14rc1"
+PKG_VERSION="2.7.14"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.python.org/"
