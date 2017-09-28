c9e8f0abeb1c02f7e4f79c7505409605468453d8
escalade <trondah@gmail.com>
2017-09-28 10:49:37 +0200

    libretro-database: update to 2136f96

--- packages/escalade/emulation/libretro-database/package.mk
+++ packages/escalade/emulation/libretro-database/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="libretro-database"
-PKG_VERSION="525e1f5"
+PKG_VERSION="2136f96"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/libretro/libretro-database"
