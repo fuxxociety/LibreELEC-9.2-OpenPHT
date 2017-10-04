d2cf9e51f0b4a9d374c697f7e21344d3b128a3ac
escalade <trondah@gmail.com>
2017-10-04 11:29:22 +0200

    retroarch: update to 0321095

--- packages/escalade/emulation/retroarch/package.mk
+++ packages/escalade/emulation/retroarch/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="retroarch"
-PKG_VERSION="c828a42"
+PKG_VERSION="0321095"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/libretro/RetroArch.git"
