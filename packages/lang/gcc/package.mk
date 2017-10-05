f8e1aebf844768b20bea7b427e378af2c43b034c
escalade <trondah@gmail.com>
2017-10-05 14:28:45 +0200

    gcc: enable libatomic

--- packages/lang/gcc/package.mk
+++ packages/lang/gcc/package.mk
@@ -83,6 +83,7 @@ PKG_CONFIGURE_OPTS_HOST="$GCC_COMMON_CONFIGURE_OPTS \
                          --disable-libstdcxx-pch \
                          --enable-libstdcxx-time \
                          --enable-clocale=gnu \
+                         --enable-libatomic \
                          $GCC_OPTS"
 
 pre_configure_host() {
@@ -148,6 +149,7 @@ makeinstall_target() {
     cp -P $PKG_BUILD/.$HOST_NAME/$TARGET_NAME/libgcc/libgcc_s.so* $INSTALL/usr/lib
     cp -P $PKG_BUILD/.$HOST_NAME/$TARGET_NAME/libgomp/.libs/libgomp.so* $INSTALL/usr/lib
     cp -P $PKG_BUILD/.$HOST_NAME/$TARGET_NAME/libstdc++-v3/src/.libs/libstdc++.so* $INSTALL/usr/lib
+    cp -P $PKG_BUILD/.$HOST_NAME/$TARGET_NAME/libatomic/.libs/libatomic.so* $INSTALL/usr/lib
 }
 
 configure_init() {
