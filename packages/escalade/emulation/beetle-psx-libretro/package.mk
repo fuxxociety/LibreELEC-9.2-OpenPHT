52bab00ab51a8095efc78fc22ca1bb7672587e20
escalade <trondah@gmail.com>
2017-10-24 11:45:00 +0200

    beetle-psx-libretro: update to 8fb566d

--- packages/escalade/emulation/beetle-psx-libretro/package.mk
+++ packages/escalade/emulation/beetle-psx-libretro/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="beetle-psx-libretro"
-PKG_VERSION="b6a39e9"
+PKG_VERSION="8fb566d"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/beetle-psx-libretro"
