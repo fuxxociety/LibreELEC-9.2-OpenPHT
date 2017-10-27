e9e00926aa33a900e7dede12ae3d49b2230b427f
escalade <trondah@gmail.com>
2017-10-27 10:18:15 +0200

    libnfs: update to 5bc77e9

--- packages/network/libnfs/package.mk
+++ packages/network/libnfs/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="libnfs"
-PKG_VERSION="3ba2a2c"
+PKG_VERSION="5bc77e9"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/sahlberg/libnfs"
