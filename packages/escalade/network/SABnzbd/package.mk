065b35642ec4a36ce4d4d4e8d2b6ab5935453f2d
escalade <trondah@gmail.com>
2017-11-09 09:19:44 +0100

    SABnzbd: update to 2.3.1

--- packages/escalade/network/SABnzbd/package.mk
+++ packages/escalade/network/SABnzbd/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="SABnzbd"
-PKG_VERSION="2.3.0"
+PKG_VERSION="2.3.1"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://sabnzbd.org/"
