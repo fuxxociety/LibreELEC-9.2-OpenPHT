888ca3fec4af4ef9ecb3047d272c0dc9964c1075
escalade <trondah@gmail.com>
2017-11-09 09:16:36 +0100

    emulation: package bumps

--- packages/escalade/emulation/retroarch-assets/package.mk
+++ packages/escalade/emulation/retroarch-assets/package.mk
@@ -19,7 +19,7 @@
 ################################################################################
 
 PKG_NAME="retroarch-assets"
-PKG_VERSION="3acffa4"
+PKG_VERSION="e0beb49"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/libretro/retroarch-assets"
