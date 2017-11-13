d9046c87bec8e78df94586518fe09ec763bb66eb
escalade <trondah@gmail.com>
2017-11-13 13:27:10 +0100

    iwlwifi-firmware: update to 78b9e19

--- packages/linux-firmware/iwlwifi-firmware/package.mk
+++ packages/linux-firmware/iwlwifi-firmware/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="iwlwifi-firmware"
-PKG_VERSION="c68485d"
+PKG_VERSION="78b9e19"
 PKG_ARCH="any"
 PKG_LICENSE="Free-to-use"
 PKG_SITE="https://github.com/LibreELEC/iwlwifi-firmware"
