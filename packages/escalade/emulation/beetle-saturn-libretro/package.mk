76efdf4314466e0eaecfbfd4b10d1439d023341a
escalade <trondah@gmail.com>
2017-11-21 12:36:33 +0100

    emulation: package bumps for beetle-psx-libretro, beetle-saturn-libretro and ppsspp-libretro

--- packages/escalade/emulation/beetle-saturn-libretro/package.mk
+++ packages/escalade/emulation/beetle-saturn-libretro/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="beetle-saturn-libretro"
-PKG_VERSION="c6397d2"
+PKG_VERSION="11336f3"
 PKG_ARCH="x86_64"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/beetle-saturn-libretro"
--- packages/escalade/emulation/ppsspp-libretro/package.mk
+++ packages/escalade/emulation/ppsspp-libretro/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="ppsspp-libretro"
-PKG_VERSION="d6fe312"
+PKG_VERSION="8672e0c"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/ppsspp"
