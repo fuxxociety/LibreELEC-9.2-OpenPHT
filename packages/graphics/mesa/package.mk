39029c2f8fc194ca1035dbbf8e3512ff88cd9450
escalade <trondah@gmail.com>
2017-12-05 10:48:03 +0100

    mesa: update to 30abe7d

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="mesa"
-PKG_VERSION="d1e6cf4"
+PKG_VERSION="30abe7d"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
