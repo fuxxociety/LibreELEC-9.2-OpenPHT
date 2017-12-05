9d45fecb7a42b7eadd3059e7336bf9eb6c5e4110
escalade <trondah@gmail.com>
2017-12-05 14:27:46 +0100

    util-linux: update to 2.31

--- packages/sysutils/util-linux/package.mk
+++ packages/sysutils/util-linux/package.mk
@@ -17,10 +17,10 @@
 ################################################################################
 
 PKG_NAME="util-linux"
-PKG_VERSION="2.30.2"
+PKG_VERSION="2.31"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_URL="http://www.kernel.org/pub/linux/utils/util-linux/v2.30/$PKG_NAME-$PKG_VERSION.tar.xz"
+PKG_URL="http://www.kernel.org/pub/linux/utils/util-linux/v2.31/$PKG_NAME-$PKG_VERSION.tar.xz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_DEPENDS_INIT="toolchain"
 PKG_SECTION="system"
