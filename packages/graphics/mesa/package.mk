2f3479fbaccc9e3fe73db1b6409bef7aa67fb75c
escalade <trondah@gmail.com>
2017-12-19 10:37:47 +0100

    mesa: update to 09215b2

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="mesa"
-PKG_VERSION="30abe7d"
+PKG_VERSION="09215b2"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
