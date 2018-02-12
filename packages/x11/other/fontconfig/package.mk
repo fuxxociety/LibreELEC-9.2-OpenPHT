a1ff5c1136c54d086c5d800b07563d9ee15f91b3
escalade <trondah@gmail.com>
2018-02-12 14:10:38 +0100

    fontconfig: update to 2.12.6

--- packages/x11/other/fontconfig/package.mk
+++ packages/x11/other/fontconfig/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="fontconfig"
-PKG_VERSION="2.12.1"
+PKG_VERSION="2.12.6"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.fontconfig.org"
