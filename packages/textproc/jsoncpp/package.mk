d21303b806d24966987b855ba59c71c528966ab2
escalade <trondah@gmail.com>
2018-02-20 13:18:27 +0100

    jsoncpp: update to 1.8.3

--- packages/textproc/jsoncpp/package.mk
+++ packages/textproc/jsoncpp/package.mk
@@ -17,23 +17,21 @@
 ################################################################################
 
 PKG_NAME="jsoncpp"
-PKG_VERSION="src-0.5.0"
+PKG_VERSION="1.8.3"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
-PKG_SITE="http://www.kodi.tv"
-PKG_URL="http://sources.openelec.tv/mirror/jsoncpp/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SITE="https://github.com/open-source-parsers/jsoncpp/"
+PKG_URL="https://github.com/open-source-parsers/jsoncpp/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="multimedia"
-PKG_SHORTDESC="jsoncpp"
-PKG_LONGDESC="jsoncpp"
+PKG_SHORTDESC="A C++ library for interacting with JSON."
+PKG_LONGDESC="A C++ library for interacting with JSON."
 
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
+PKG_CMAKE_OPTS_TARGET="-DJSONCPP_WITH_TESTS=OFF"
+
 pre_configure_target() {
   export CFLAGS="$CFLAGS -fPIC"
-}
-
-pre_build_target() {
-  cp $PKG_DIR/config/CMakeLists.txt $PKG_BUILD
 }
