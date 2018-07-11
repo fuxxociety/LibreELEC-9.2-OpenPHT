397df5fe44cc94e2fe35512fc5df2a32e6821f13
escalade <trondah@gmail.com>
2018-07-12 00:02:34 +0200

    mame2003-plus-libretro: initial package

---
+++ packages/escalade/emulation/mame2003-plus-libretro/package.mk
@@ -0,0 +1,27 @@
+# Made by github.com/escalade
+#
+PKG_NAME="mame2003-plus-libretro"
+PKG_VERSION="e145183"
+PKG_ARCH="any"
+PKG_LICENSE="MAME"
+PKG_SITE="https://github.com/libretro/mame2003-plus-libretro"
+PKG_URL="https://github.com/libretro/mame2003-plus-libretro/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="emulation"
+PKG_SHORTDESC="MAME - Multiple Arcade Machine Emulator"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+configure_target() {
+  export LD=$CC
+}
+
+make_target() {
+  make GIT_VERSION=$PKG_VERSION
+}
+
+makeinstall_target() {
+  mkdir -p $INSTALL/usr/lib/libretro
+  cp *.so $INSTALL/usr/lib/libretro/
+}
