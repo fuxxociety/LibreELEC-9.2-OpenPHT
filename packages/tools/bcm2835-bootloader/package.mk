1cf9fd0b829963a8e5c80d6ad0fc39f2498bc635
escalade <trondah@gmail.com>
2018-07-19 12:37:24 +0200

    RPi: update firmware to bffe7ee

--- packages/tools/bcm2835-bootloader/package.mk
+++ packages/tools/bcm2835-bootloader/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="bcm2835-bootloader"
-PKG_VERSION="d3c3d03"
+PKG_VERSION="bffe7ee"
 PKG_ARCH="arm"
 PKG_LICENSE="nonfree"
 PKG_SITE="http://www.broadcom.com"
