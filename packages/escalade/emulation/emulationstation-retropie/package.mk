fba20d4f6d820c89171226eba603e60e24a1756b
escalade <trondah@gmail.com>
2018-07-23 14:29:13 +0200

    emulationstation-retropie: initial package

--- packages/escalade/emulation/emulationstation-retropie/package.mk
+++ packages/escalade/emulation/emulationstation-retropie/package.mk
@@ -1,12 +1,12 @@
 # Made by github.com/escalade
 #
 PKG_NAME="emulationstation-retropie"
-PKG_VERSION="920db2b"
+PKG_VERSION="de021ca"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/RetroPie/EmulationStation"
 PKG_SOURCE_DIR="EmulationStation-$PKG_VERSION*"
-PKG_DEPENDS_TARGET="toolchain SDL2 boost freetype curl cmake:host freeimage eigen emulationstation-theme-simple-dark vlc"
+PKG_DEPENDS_TARGET="toolchain SDL2 freetype curl cmake:host freeimage emulationstation-theme-carbon vlc"
 PKG_SECTION="emulation"
 PKG_SHORTDESC="Emulationstation emulator frontend"
 
@@ -39,6 +39,7 @@ post_makeinstall_target() {
     cp $PKG_DIR/files/emulationstation.conf $INSTALL/usr/config/emulationstation/
     cp $PKG_DIR/files/es_input.cfg $INSTALL/usr/config/emulationstation/
     cp $PKG_DIR/files/es_settings.cfg $INSTALL/usr/config/emulationstation/
+    cp -a ../resources $INSTALL/usr/config/emulationstation/
     if [[ "$PROJECT" =~ "RPi" ]]; then
       cp $PKG_DIR/files/es_systems-rpi.cfg $INSTALL/usr/config/emulationstation/es_systems.cfg
       cp $PKG_DIR/files/emulationstation-userdirs-rpi.conf $INSTALL/usr/lib/tmpfiles.d/emulationstation-userdirs.conf
