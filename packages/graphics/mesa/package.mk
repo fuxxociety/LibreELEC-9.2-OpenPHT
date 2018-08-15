4d09eaea703ec9811ddafed3a821099ab41cfde4
escalade <trondah@gmail.com>
2018-08-15 12:06:25 +0200

    mesa: update to 4a25d8b (18.2.0-rc2)

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="mesa"
-PKG_VERSION="4cf8f32"
+PKG_VERSION="4a25d8b"
 PKG_ARCH="any"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
