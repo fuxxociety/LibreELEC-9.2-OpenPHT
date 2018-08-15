80d814ef5db20d297d3750469d916b10625ee145
escalade <trondah@gmail.com>
2018-08-15 16:24:06 +0200

    linux: update to 4.18 for generic/rpi

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -59,13 +59,13 @@ case "$LINUX" in
     PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET imx6-status-led imx6-soc-fan irqbalanced"
     ;;
   default-rpi)
-    # 4.17.13
-    PKG_VERSION="a1cb21b"
+    # 4.18.0
+    PKG_VERSION="9c00ec7"
     PKG_URL="https://github.com/raspberrypi/linux/archive/$PKG_VERSION.tar.gz"
     PKG_PATCH_DIRS="default-rpi"
     ;;
   *)
-    PKG_VERSION="4.17.14"
+    PKG_VERSION="4.18"
     PKG_URL="https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-$PKG_VERSION.tar.xz"
     PKG_PATCH_DIRS="default"
     ;;
