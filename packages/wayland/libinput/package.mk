d1158e959d867ed6c1cddc8bdbc58a7cd6736cd1
Trond Haugland <escalade@users.noreply.github.com>
2019-04-12 21:50:01 +0200

    libinput: update to 1.13.1

--- packages/wayland/libinput/package.mk
+++ packages/wayland/libinput/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="libinput"
-PKG_VERSION="1.10.4"
-PKG_SHA256="2330a82f3e4b617a4f9ff0f2bf9cc217b3c4e69d767e61ca59ca07add61f68ac"
+PKG_VERSION="1.13.1"
+PKG_SHA256="e1d487d478ee2bb118890a64211c2b7f2348aaed7363b214bed4d642274841ab"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.freedesktop.org/wiki/Software/libinput/"
 PKG_URL="http://www.freedesktop.org/software/libinput/$PKG_NAME-$PKG_VERSION.tar.xz"
