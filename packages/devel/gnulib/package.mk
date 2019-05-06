1fc54924047260b33aff9dae2d29b03a38fba78a
MilhouseVH <milhouseVH.github@nmacleod.com>
2019-05-06 04:25:42 +0100

    gnulib: new package - required by grub-2.04

---
+++ packages/devel/gnulib/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2019-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="gnulib"
+PKG_VERSION="620857c4dccb8f0548cdb855f6b2f46ebea3919e"
+PKG_SHA256="4f7fa66538555a40b69f26dc56191178be75d80f20827aa3653516da5ce824bb"
+PKG_LICENSE="GPL"
+PKG_SITE="https://savannah.gnu.org/git/?group=gnulib"
+PKG_URL="http://git.savannah.gnu.org/cgit/gnulib.git/snapshot/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_LONGDESC="GNU portability library"
+PKG_TOOLCHAIN="manual"
