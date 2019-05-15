92469dd7e6d942aa27a3d371b94a6d215cb44d68
Trond Haugland <escalade@users.noreply.github.com>
2019-05-15 12:57:05 +0200

    libcec: disable LTO (caused ES to crash when returning from a game)

--- packages/devel/libcec/package.mk
+++ packages/devel/libcec/package.mk
@@ -10,6 +10,7 @@ PKG_SITE="http://libcec.pulse-eight.com/"
 PKG_URL="https://github.com/Pulse-Eight/libcec/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain systemd p8-platform swig:host"
 PKG_LONGDESC="libCEC is an open-source dual licensed library designed for communicating with the Pulse-Eight USB - CEC Adaptor."
+PKG_BUILD_FLAGS="-lto"
 
 PKG_CMAKE_OPTS_TARGET="-DBUILD_SHARED_LIBS=1 \
                        -DCMAKE_INSTALL_LIBDIR:STRING=lib \
