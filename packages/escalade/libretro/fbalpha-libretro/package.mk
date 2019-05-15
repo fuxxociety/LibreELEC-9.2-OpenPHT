5f482e2b5b371be48bd8326d9d6e65c121a51a70
Trond Haugland <escalade@users.noreply.github.com>
2019-05-15 13:21:52 +0200

    fbalpha-libretro: update to e8b51b7 (new fork)

--- packages/escalade/libretro/fbalpha-libretro/package.mk
+++ packages/escalade/libretro/fbalpha-libretro/package.mk
@@ -2,20 +2,19 @@
 # Copyright (C) 2019 Trond Haugland (trondah@gmail.com)
 
 PKG_NAME="fbalpha-libretro"
-PKG_VERSION="5c31f8c"
+PKG_VERSION="e8b51b7"
 PKG_ARCH="any"
 PKG_LICENSE="Non-commercial"
-PKG_SITE="https://github.com/libretro/fbalpha"
-PKG_URL="https://github.com/libretro/fbalpha/archive/$PKG_VERSION.tar.gz"
-PKG_SOURCE_DIR="fbalpha-$PKG_VERSION*"
+PKG_SITE="https://github.com/libretro/FBAlphaFB"
+PKG_URL="https://github.com/libretro/FBAlphaFB/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="escalade"
 PKG_SHORTDESC="Port of Final Burn Alpha to Libretro."
 PKG_TOOLCHAIN="make"
-PKG_BUILD_FLAGS="-lto"
+PKG_BUILD_FLAGS="+lto"
 
 pre_make_target() {
-  PKG_MAKE_OPTS_TARGET="-f makefile.libretro CC=$CC CXX=$CXX GIT_VERSION=$PKG_VERSION"
+  PKG_MAKE_OPTS_TARGET="-C src/burner/libretro CC=$CC CXX=$CXX GIT_VERSION=$PKG_VERSION"
 
   if [ "$PROJECT" = "RPi" ]; then
     case $DEVICE in
@@ -36,5 +35,5 @@ pre_make_target() {
 
 makeinstall_target() {
   mkdir -p $INSTALL/usr/lib/libretro
-  cp fbalpha_libretro.so $INSTALL/usr/lib/libretro/
+  cp src/burner/libretro/fbalpha_libretro.so $INSTALL/usr/lib/libretro/
 }
