a75af6bce8b954947df16eac657cc0908a15888b
Trond Haugland <escalade@users.noreply.github.com>
2019-05-26 20:27:31 +0200

    libnsl: initial package

---
+++ packages/escalade/depends/libnsl/package.mk
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2019 Trond Haugland (trondah@gmail.com)
+
+PKG_NAME="libnsl"
+PKG_VERSION="1.2.0"
+PKG_SHA256="a5a28ef17c4ca23a005a729257c959620b09f8c7f99d0edbfe2eb6b06bafd3f8"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/thkukuk/libnsl"
+PKG_URL="https://github.com/thkukuk/libnsl/archive/v$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_SECTION="escalade"
+PKG_LONGDESC="This library contains the public client interface for NIS(YP) and NIS+ in a IPv6 ready version"
+PKG_TOOLCHAIN="autotools"
