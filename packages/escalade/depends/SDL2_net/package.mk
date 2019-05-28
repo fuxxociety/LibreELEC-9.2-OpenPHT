684b40e77a5eafc90f5d0873ccafbbb78d909a4f
Trond Haugland <escalade@users.noreply.github.com>
2019-05-28 18:03:16 +0200

    SDL_net: updated to 6fb77962243c and renamed to SDL2_net

--- packages/escalade/depends/SDL2_net/package.mk
+++ packages/escalade/depends/SDL2_net/package.mk
@@ -2,11 +2,12 @@
 # Copyright (C) 2019 Trond Haugland (trondah@gmail.com)
 
 PKG_NAME="SDL2_net"
-PKG_VERSION="2.0.1"
+PKG_VERSION="6fb77962243c"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="https://www.libsdl.org/projects/SDL_net/"
 PKG_URL="https://www.libsdl.org/projects/SDL_net/release/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_URL="http://hg.libsdl.org/SDL_net/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain SDL2"
 PKG_SECTION="escalade"
 PKG_SHORTDESC="SDL2_net: network library" 
