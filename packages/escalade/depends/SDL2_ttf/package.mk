cfaab8fa08bb62a91e9b460258082988f8f307f8
Trond Haugland <escalade@users.noreply.github.com>
2019-05-28 13:07:37 +0200

    SDL2_ttf: switch to mercurial and use cmake

--- packages/escalade/depends/SDL2_ttf/package.mk
+++ packages/escalade/depends/SDL2_ttf/package.mk
@@ -2,12 +2,12 @@
 # Copyright (C) 2019 Trond Haugland (trondah@gmail.com)
 
 PKG_NAME="SDL2_ttf"
-PKG_VERSION="2.0.15"
+PKG_VERSION="049abc1f36b5"
+PKG_SHA256="41babf417e9abd067ca80063ca141133d6a0f9484c7511db0edb22f75da8d57e"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.libsdl.org/"
-PKG_URL="https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain SDL2"
+PKG_URL="http://hg.libsdl.org/SDL_ttf/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain SDL2 freetype"
 PKG_SECTION="escalade"
 PKG_SHORTDESC="This is a sample library which allows you to use TrueType fonts in your SDL applications"
-PKG_TOOLCHAIN="configure"
