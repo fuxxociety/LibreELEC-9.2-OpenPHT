30f24064bec910c3eeb5aa31d09dd96037ecc184
Trond Haugland <escalade@users.noreply.github.com>
2019-05-28 14:49:44 +0200

    SDL_sound: update to 9262f9205898 and depend on SDL2

--- packages/escalade/depends/SDL_sound/package.mk
+++ packages/escalade/depends/SDL_sound/package.mk
@@ -2,20 +2,23 @@
 # Copyright (C) 2019 Trond Haugland (trondah@gmail.com)
 
 PKG_NAME="SDL_sound"
-PKG_VERSION="719dade41745"
+PKG_VERSION="9262f9205898"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://www.icculus.org/SDL_sound/"
 PKG_URL="http://hg.icculus.org/icculus/SDL_sound/archive/$PKG_VERSION.tar.gz"
-PKG_DEPENDS_TARGET="toolchain alsa-lib SDL"
+PKG_DEPENDS_TARGET="toolchain alsa-lib SDL2"
 PKG_SECTION="escalade"
 PKG_SHORTDESC="SDL_sound library"
-PKG_TOOLCHAIN="autotools"
 
-PKG_CONFIGURE_OPTS_TARGET="--prefix=/usr \
-			   --disable-speex \
-			   ac_cv_path_SDL2_CONFIG=$SYSROOT_PREFIX/usr/bin/sdl2-config"
+PKG_CMAKE_OPTS_TARGET="-DSDLSOUND_BUILD_STATIC=off"
 
-post_unpack() {
-  touch $PKG_BUILD/README
+pre_configure_target() {
+  export CFLAGS="$CFLAGS -I$SYSROOT_PREFIX/usr/include/SDL2"
+  export LDFLAGS="$LDFLAGS -lSDL2 -lm"
+}
+
+post_makeinstall_target() {
+  ln -s libSDL2_sound.so $INSTALL/usr/lib/libSDL_sound.so
+  test -f $SYSROOT_PREFIX/usr/lib/libSDL_sound.so || ln -s libSDL2_sound.so $SYSROOT_PREFIX/usr/lib/libSDL_sound.so
 }
