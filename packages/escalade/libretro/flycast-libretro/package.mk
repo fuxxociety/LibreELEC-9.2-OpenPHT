2bd056060a92892fbd6ae8cbe30cc7c5af0a1c65
Trond Haugland <escalade@users.noreply.github.com>
2019-06-07 11:27:23 +0200

    beetle-dc-libretro: rename to flycast-libretro

--- packages/escalade/libretro/flycast-libretro/package.mk
+++ packages/escalade/libretro/flycast-libretro/package.mk
@@ -1,37 +1,33 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2019 Trond Haugland (trondah@gmail.com)
 
-PKG_NAME="beetle-dc-libretro"
-PKG_VERSION="78bc44a"
-PKG_ARCH="x86_64 arm"
+PKG_NAME="flycast-libretro"
+PKG_VERSION="4a020d4"
+PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
-PKG_SITE="https://github.com/libretro/beetle-dc"
-PKG_URL="https://github.com/libretro/beetle-dc/archive/$PKG_VERSION.tar.gz"
+PKG_SITE="https://github.com/libretro/flycast"
+PKG_URL="https://github.com/libretro/flycast/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="escalade"
-PKG_SHORTDESC="Reicast is a multiplatform Sega Dreamcast emulator"
+PKG_SHORTDESC="Flycast is a multiplatform Sega Dreamcast emulator"
 PKG_TOOLCHAIN="manual"
 PKG_BUILD_FLAGS="-gold"
 
 make_target() {
-  mkdir -p out
   case $PROJECT in
     RPi)
       make GIT_VERSION=$PKG_VERSION WITH_DYNAREC=$ARCH FORCE_GLES=1 platform=rpi2
-      mv *.so out
       ;;
     OdroidXU3)
       make GIT_VERSION=$PKG_VERSION WITH_DYNAREC=$ARCH platform=odroid BOARD=ODROID-XU4
-      mv *.so out
       ;;
     Generic)
-      make AS=${AS} CC_AS=${AS} HAVE_OIT=1 GIT_VERSION=$PKG_VERSION WITH_DYNAREC=$ARCH
-      mv *.so out
+      make AS=$AS CC_AS=$AS HAVE_OIT=1 GIT_VERSION=$PKG_VERSION WITH_DYNAREC=$ARCH
       ;;
   esac
 }
 
 makeinstall_target() {
   mkdir -p $INSTALL/usr/lib/libretro
-  cp out/*.so $INSTALL/usr/lib/libretro/
+  cp *.so $INSTALL/usr/lib/libretro/
 }
