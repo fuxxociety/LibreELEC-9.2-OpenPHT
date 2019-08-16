bb21c3e9d60fedb06cd7d1cfd1ac2c801e7c40ea
Trond Haugland <escalade@users.noreply.github.com>
2019-08-16 11:29:53 +0200

    samba: update to 4.9.11

--- packages/network/samba/package.mk
+++ packages/network/samba/package.mk
@@ -3,8 +3,8 @@
 # Copyright (C) 2017-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="samba"
-PKG_VERSION="4.9.8"
-PKG_SHA256="82ebb7c3f1847c39341dd97ff8b73f40fa83f5f794daeceb80f3c349ace3cf56"
+PKG_VERSION="4.9.11"
+PKG_SHA256="bb736624d16f7369e395de2f15fec153b554f76f95864015b4ce1f2ae53e817b"
 PKG_LICENSE="GPLv3+"
 PKG_SITE="https://www.samba.org"
 PKG_URL="https://download.samba.org/pub/samba/stable/$PKG_NAME-$PKG_VERSION.tar.gz"
