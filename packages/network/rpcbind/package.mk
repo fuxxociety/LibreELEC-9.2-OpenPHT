6884ba8ac9ac8bdd1406a3602e5648b5f3e50ee1
Trond Haugland <escalade@users.noreply.github.com>
2019-08-16 10:50:50 +0200

    rpcbind: fixup

--- packages/network/rpcbind/package.mk
+++ packages/network/rpcbind/package.mk
@@ -8,13 +8,16 @@ PKG_SHA256="2ce360683963b35c19c43f0ee2c7f18aa5b81ef41c3fdbd15ffcb00b8bffda7a"
 PKG_LICENSE="OSS"
 PKG_SITE="http://rpcbind.sourceforge.net/"
 PKG_URL="$SOURCEFORGE_SRC/rpcbind/rpcbind/$PKG_VERSION/$PKG_NAME-$PKG_VERSION.tar.bz2"
-PKG_DEPENDS_TARGET="toolchain libtirpc systemd libnsl"
+PKG_DEPENDS_TARGET="toolchain libtirpc systemd"
 PKG_LONGDESC="The rpcbind utility is a server that converts RPC program numbers into universal addresses."
 
-PKG_CONFIGURE_OPTS_TARGET="--disable-libwrap \
+PKG_CONFIGURE_OPTS_TARGET="ac_cv_header_rpcsvc_mount_h=no \
+                           --disable-libwrap \
+                           --enable-rmtcalls \
                            --with-statedir=/tmp \
                            --with-rpcuser=root"
 
 post_install() {
+  enable_service rpcbind.socket
   enable_service rpcbind.service
 }
