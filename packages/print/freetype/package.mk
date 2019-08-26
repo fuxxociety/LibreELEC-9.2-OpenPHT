a3d81e658e8e8581307db63f2d20817e21c4c281
Trond Haugland <escalade@users.noreply.github.com>
2019-08-26 20:22:31 +0200

    freetype: update to 2.10.1

--- packages/print/freetype/package.mk
+++ packages/print/freetype/package.mk
@@ -3,11 +3,11 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="freetype"
-PKG_VERSION="2.10.0"
-PKG_SHA256="fccc62928c65192fff6c98847233b28eb7ce05f12d2fea3f6cc90e8b4e5fbe06"
+PKG_VERSION="2.10.1"
+PKG_SHA256="3a60d391fd579440561bf0e7f31af2222bc610ad6ce4d9d7bd2165bca8669110"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.freetype.org"
-PKG_URL="http://download.savannah.gnu.org/releases/freetype/$PKG_NAME-$PKG_VERSION.tar.bz2"
+PKG_URL="http://download.savannah.gnu.org/releases/freetype/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_HOST="gcc:host"
 PKG_DEPENDS_TARGET="toolchain zlib libpng"
 PKG_LONGDESC="The FreeType engine is a free and portable TrueType font rendering engine."
@@ -19,6 +19,8 @@ PKG_CONFIGURE_OPTS_TARGET="LIBPNG_CFLAGS=-I$SYSROOT_PREFIX/usr/include \
                            --with-zlib"
 
 pre_configure_target() {
+  export CFLAGS=`echo $CFLAGS | sed -e "s|-O.|-O3|g"`
+  export CXXFLAGS=`echo $CFLAGS | sed -e "s|-O.|-O3|g"`
   # unset LIBTOOL because freetype uses its own
     ( cd ..
       unset LIBTOOL
