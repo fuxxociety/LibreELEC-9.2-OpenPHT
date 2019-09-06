5b11df6350cc83899510081ffef590ae483d46b8
Trond Haugland <escalade@users.noreply.github.com>
2019-09-06 19:36:18 +0200

    linux: update to 5.2.13

--- packages/linux/package.mk
+++ packages/linux/package.mk
@@ -37,8 +37,8 @@ case "$LINUX" in
     PKG_BUILD_PERF="no"
     ;;
   *)
-    PKG_VERSION="5.2.12"
-    PKG_SHA256="ffc443b2dc9f9574d1ff142bf15dbfee46bb81a00b3932923622265a97bd5eaf"
+    PKG_VERSION="5.2.13"
+    PKG_SHA256="17b60f55241dee4b9a2919a653de144ef1002e2de49ccf5d15225b1f07bc178a"
     PKG_URL="https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-$PKG_VERSION.tar.xz"
     PKG_PATCH_DIRS="default"
     PKG_BUILD_PERF="no"
