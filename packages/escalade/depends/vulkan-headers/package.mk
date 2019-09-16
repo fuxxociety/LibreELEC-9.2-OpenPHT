440370db8610524c7dfb4cb0c0033a12cccce0f1
Trond Haugland <escalade@users.noreply.github.com>
2019-09-16 15:52:59 +0200

    vulkan-headers: update to 1.1.122

--- packages/escalade/depends/vulkan-headers/package.mk
+++ packages/escalade/depends/vulkan-headers/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019 Trond Haugland (trondah@gmail.com)
 
 PKG_NAME="vulkan-headers"
-PKG_VERSION="1.1.120"
-PKG_SHA256="6bbfc35c1a826930e22ff39a99175242e293522632011c1a7d6f0e2d509d82bf"
+PKG_VERSION="1.1.122"
+PKG_SHA256="00874cff60bb167c9ee4a60d6f17107e082a44dacaacf9e6a9da5fd2cca1f3c8"
 PKG_LICENSE="Apache 2.0"
 PKG_SITE="https://github.com/KhronosGroup/Vulkan-Headers"
 PKG_URL="https://github.com/KhronosGroup/Vulkan-Headers/archive/v${PKG_VERSION}.tar.gz"
