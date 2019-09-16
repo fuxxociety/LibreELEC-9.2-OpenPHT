cb705a1b952680dda763467b2e7a342a11b279b6
Trond Haugland <escalade@users.noreply.github.com>
2019-09-16 14:41:44 +0200

    alsa-lib: update to 1d7a131 and enable lto

--- packages/audio/alsa-lib/package.mk
+++ packages/audio/alsa-lib/package.mk
@@ -3,15 +3,15 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="alsa-lib"
-PKG_VERSION="1.1.9"
-PKG_SHA256="488373aef5396682f3a411a6d064ae0ad196b9c96269d0bb912fbdeec94b994b"
+PKG_VERSION="1d7a131"
+PKG_SHA256="77da414077d237a0fe484a4a8ee70b3094608af89f864ea248b9166bac6c1664"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.alsa-project.org/"
-PKG_URL="ftp://ftp.alsa-project.org/pub/lib/alsa-lib-$PKG_VERSION.tar.bz2"
+PKG_URL="https://github.com/alsa-project/alsa-lib/archive/$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain"
 PKG_LONGDESC="ALSA (Advanced Linux Sound Architecture) is the next generation Linux Sound API."
 PKG_TOOLCHAIN="autotools"
-PKG_BUILD_FLAGS="+pic -lto"
+PKG_BUILD_FLAGS="+pic"
 
 if build_with_debug; then
   PKG_ALSA_DEBUG=--with-debug
@@ -23,6 +23,10 @@ PKG_CONFIGURE_OPTS_TARGET="$PKG_ALSA_DEBUG \
                            --disable-dependency-tracking \
                            --with-plugindir=/usr/lib/alsa \
                            --disable-python"
+
+pre_configure_target() {
+  export CFLAGS="$CFLAGS -flto-partition=none"
+}
 
 post_configure_target() {
   sed -i 's/.*PKGLIBDIR.*/#define PKGLIBDIR ""/' include/config.h
