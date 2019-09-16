3bcb086069fbc264a954adc3c144120f81df2fe4
Trond Haugland <escalade@users.noreply.github.com>
2019-09-16 15:52:43 +0200

    vulkan-loader: update to 1.1.121

--- packages/escalade/depends/vulkan-loader/package.mk
+++ packages/escalade/depends/vulkan-loader/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019 Trond Haugland (trondah@gmail.com)
 
 PKG_NAME="vulkan-loader"
-PKG_VERSION="1.1.120"
-PKG_SHA256="5cc0bedde46fca69da21f2bc5197de5b07f1bea1b70fedd6d6724230a4ba6fb2"
+PKG_VERSION="1.1.121"
+PKG_SHA256="8966eb5a0f1503f5d8ab1b3602913285961786f7ee06bd1ff436dcce337505d3"
 PKG_LICENSE="Apache 2.0"
 PKG_SITE="https://github.com/KhronosGroup/Vulkan-Loader"
 PKG_URL="https://github.com/KhronosGroup/Vulkan-Loader/archive/v${PKG_VERSION}.tar.gz"
