52c4140a7bcc6e2eb420c2d30d3147ec3faa9f7e
Trond Haugland <escalade@users.noreply.github.com>
2019-09-17 23:51:10 +0200

    mesa: update to 99cbec0

--- packages/graphics/mesa/package.mk
+++ packages/graphics/mesa/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="mesa"
-PKG_VERSION="2bfcf04"
+PKG_VERSION="99cbec0"
 PKG_LICENSE="OSS"
 PKG_SITE="http://www.mesa3d.org/"
 PKG_URL="https://cgit.freedesktop.org/mesa/mesa/snapshot/$PKG_VERSION.tar.gz"
