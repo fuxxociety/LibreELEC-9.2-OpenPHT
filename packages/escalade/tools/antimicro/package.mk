c047f2a7cd2e690d7f48ce1ea433c4bb708518ac
Trond Haugland <escalade@users.noreply.github.com>
2019-09-17 22:30:18 +0200

    escalade: initial antimicro package

---
+++ packages/escalade/tools/antimicro/package.mk
@@ -0,0 +1,23 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2019 Trond Haugland (trondah@gmail.com)
+
+PKG_NAME="antimicro"
+PKG_VERSION="2.24.2"
+PKG_LICENSE="GPLv2"
+PKG_SITE="https://github.com/juliagoda/antimicro"
+PKG_URL="https://github.com/juliagoda/antimicro/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain SDL2 qt-everywhere extra-cmake-modules xorg-server"
+PKG_SECTION="escalade"
+PKG_SHORTDESC="Graphical program used to map keyboard buttons and mouse controls to a gamepad."
+PKG_TOOLCHAIN="cmake-make"
+
+PKG_CMAKE_OPTS_TARGET="-DAPPDATA=ON -DWITH_UINPUT=ON"
+
+post_makeinstall_target() {
+  mkdir -p $INSTALL/usr/config/antimicro
+  cp $PKG_DIR/config/* $INSTALL/usr/config/antimicro/
+}
+
+post_install() {
+  enable_service antimicro.service
+}
