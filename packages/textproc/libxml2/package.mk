f3d98707165524edc645bb39e96ee8a76e841a97
Trond <escalade@users.noreply.github.com>
2019-09-19 20:42:49 +0200

    libxml2: add python bindings for host (needed by itstool:host)

--- packages/textproc/libxml2/package.mk
+++ packages/textproc/libxml2/package.mk
@@ -17,13 +17,12 @@ PKG_CONFIGURE_OPTS_ALL="ac_cv_header_ansidecl_h=no \
              --enable-shared \
              --disable-silent-rules \
              --enable-ipv6 \
-             --without-python \
              --with-zlib=$TOOLCHAIN \
              --without-lzma"
 
-PKG_CONFIGURE_OPTS_HOST="$PKG_CONFIGURE_OPTS_ALL --with-zlib=$TOOLCHAIN"
+PKG_CONFIGURE_OPTS_HOST="$PKG_CONFIGURE_OPTS_ALL --with-zlib=$TOOLCHAIN --with-python=$TOOLCHAIN"
 
-PKG_CONFIGURE_OPTS_TARGET="$PKG_CONFIGURE_OPTS_ALL --with-zlib=$SYSROOT_PREFIX/usr --with-sysroot=$SYSROOT_PREFIX"
+PKG_CONFIGURE_OPTS_TARGET="$PKG_CONFIGURE_OPTS_ALL --with-zlib=$SYSROOT_PREFIX/usr --with-sysroot=$SYSROOT_PREFIX --without-python"
 
 post_makeinstall_target() {
   sed -e "s:\(['= ]\)/usr:\\1$SYSROOT_PREFIX/usr:g" -i $SYSROOT_PREFIX/usr/bin/xml2-config
