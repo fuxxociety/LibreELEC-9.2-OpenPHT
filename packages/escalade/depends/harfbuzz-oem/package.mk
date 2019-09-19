6a30395df6820e91a398a2d44df2461ca1c6ba24
Trond <escalade@users.noreply.github.com>
2019-09-19 21:06:55 +0200

    escalade: harfbuzz-oem: initial package

---
+++ packages/escalade/depends/harfbuzz-oem/package.mk
@@ -0,0 +1,17 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2019 Trond Haugland (trondah@gmail.com)
+
+PKG_NAME="harfbuzz-oem"
+PKG_VERSION="2.6.1"
+PKG_SHA256="c651fb3faaa338aeb280726837c2384064cdc17ef40539228d88a1260960844f"
+PKG_LICENSE="GPL"
+PKG_SITE="http://www.freedesktop.org/wiki/Software/HarfBuzz"
+PKG_URL="https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-$PKG_VERSION.tar.xz"
+PKG_DEPENDS_TARGET="toolchain cairo freetype glib"
+PKG_LONGDESC="HarfBuzz is an OpenType text shaping engine."
+PKG_TOOLCHAIN="configure"
+
+PKG_CONFIGURE_OPTS_TARGET="--with-icu=no \
+                           --disable-gtk-doc \
+                           --disable-gtk-doc-html \
+                           --disable-gtk-doc-pdf"
