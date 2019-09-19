73a1ad28a0e725521b238c92732e8a374220be05
Trond <escalade@users.noreply.github.com>
2019-09-20 00:15:26 +0200

    escalade: pango-oem: depend on cairo-oem

--- packages/escalade/depends/pango-oem/package.mk
+++ packages/escalade/depends/pango-oem/package.mk
@@ -7,7 +7,7 @@ PKG_SHA256="3e1e41ba838737e200611ff001e3b304c2ca4cdbba63d200a20db0b0ddc0f86c"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.pango.org/"
 PKG_URL="https://ftp.gnome.org/pub/gnome/sources/pango/${PKG_VERSION:0:4}/pango-$PKG_VERSION.tar.xz"
-PKG_DEPENDS_TARGET="toolchain cairo freetype fontconfig fribidi glib harfbuzz-oem libX11 libXft"
+PKG_DEPENDS_TARGET="toolchain cairo-oem freetype fontconfig fribidi glib harfbuzz-oem libX11 libXft"
 PKG_LONGDESC="The Pango library for layout and rendering of internationalized text."
 PKG_TOOLCHAIN="meson"
 
