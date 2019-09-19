da5067a6004f2c91969ee706d2680cbfc53e7225
Trond <escalade@users.noreply.github.com>
2019-09-20 00:14:37 +0200

    escalade: json-c-oem: initial package

---
+++ packages/escalade/depends/json-c-oem/package.mk
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2019 Trond Haugland (trondah@gmail.com)
+
+PKG_NAME="json-c-oem"
+PKG_VERSION="0.13.1-20180305"
+PKG_LICENSE="MIT"
+PKG_SITE="https://github.com/json-c/json-c"
+PKG_URL="https://github.com/json-c/json-c/archive/json-c-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="Implements a reference counting object model that allows you to easily construct JSON objects in C."
