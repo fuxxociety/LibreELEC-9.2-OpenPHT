df193f3d95c4df5d010ee66c91a8723a34e5a7d9
Trond <escalade@users.noreply.github.com>
2019-09-19 21:07:42 +0200

    escalade: xcb-util-cursor: initial package

---
+++ packages/escalade/depends/xcb-util-cursor/package.mk
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2019 Trond Haugland (trondah@gmail.com)
+
+PKG_NAME="xcb-util-cursor"
+PKG_VERSION="0.1.3"
+PKG_SHA256="a322332716a384c94d3cbf98f2d8fe2ce63c2fe7e2b26664b6cea1d411723df8"
+PKG_LICENSE="GPL"
+PKG_SITE="https://xcb.freedesktop.org"
+PKG_URL="https://xcb.freedesktop.org/dist/xcb-util-cursor-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain xcb-util-renderutil xcb-util-image"
