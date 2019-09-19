c65ca906a6c0c7ecec32d11a77fd9ac3f9cc5abf
Trond <escalade@users.noreply.github.com>
2019-09-19 20:34:28 +0200

    escalade: initial itstool package (needed by antimicro)

---
+++ packages/escalade/tools/itstool/package.mk
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2019 Trond Haugland (trondah@gmail.com)
+
+PKG_NAME="itstool"
+PKG_VERSION="2.0.6"
+PKG_SHA256="bda0b08e9a1db885c9d7d1545535e9814dd8931d5b8dd5ab4a47bd769d0130c6"
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://itstool.org"
+PKG_URL="https://github.com/itstool/itstool/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_HOST="toolchain Python2:host libxml2:host"
+PKG_SECTION="escalade"
+PKG_SHORTDESC="XML to PO and back again"
+PKG_TOOLCHAIN="autotools"
