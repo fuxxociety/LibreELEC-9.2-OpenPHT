45d6c4b8c53762130cfb9cefcfbe84b10ed20a70
Trond <escalade@users.noreply.github.com>
2019-09-19 21:00:07 +0200

    vulkan-loader: update to 1.1.123

--- packages/escalade/depends/vulkan-loader/package.mk
+++ packages/escalade/depends/vulkan-loader/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2019 Trond Haugland (trondah@gmail.com)
 
 PKG_NAME="vulkan-loader"
-PKG_VERSION="1.1.121"
-PKG_SHA256="8966eb5a0f1503f5d8ab1b3602913285961786f7ee06bd1ff436dcce337505d3"
+PKG_VERSION="1.1.123"
+PKG_SHA256="9d3ddb84c21c41b259bbd59e649732f0c06b7bb9c42b27311851680bace0ddde"
 PKG_LICENSE="Apache 2.0"
 PKG_SITE="https://github.com/KhronosGroup/Vulkan-Loader"
 PKG_URL="https://github.com/KhronosGroup/Vulkan-Loader/archive/v${PKG_VERSION}.tar.gz"
@@ -12,3 +12,7 @@ PKG_LONGDESC="Vulkan Installable Client Driver (ICD) Loader"
 
 PKG_CMAKE_OPTS_TARGET="-DBUILD_TESTS=Off \
                        -DBUILD_WSI_WAYLAND_SUPPORT=Off"
+
+pre_make_target() {
+  sed -i -e 's#asm_offset GAS#\./asm_offset GAS#' build.ninja
+}
