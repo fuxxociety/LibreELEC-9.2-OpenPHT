810e058a0e90a6120c24fe8528b57a5ea5b116ce
Trond <escalade@users.noreply.github.com>
2019-09-19 21:08:12 +0200

    escalade: xcb-xrm: initial package

---
+++ packages/escalade/depends/xcb-xrm/package.mk
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2019 Trond Haugland (trondah@gmail.com)
+
+PKG_NAME="xcb-xrm"
+PKG_VERSION="1.3"
+PKG_SHA256="0129f74c327ae65e2f4ad4002f300b4f02c9aff78c00997f1f1c5a430f922f34"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/Airblader/xcb-util-xrm"
+PKG_URL="https://github.com/Airblader/xcb-util-xrm/releases/download/v$PKG_VERSION/xcb-util-xrm-$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain libxcb"
+PKG_TOOLCHAIN="configure"
